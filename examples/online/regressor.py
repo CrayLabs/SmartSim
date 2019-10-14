f99f89f570e4199577e438535a91f87a2e3b613b
Sam Partee <partees21@gmail.com>
2019-10-14 18:03:55 -0500

    Fix online training example

--- examples/online/regressor.py
+++ examples/online/regressor.py
@@ -12,13 +12,13 @@ class TrainingNode():
 
     def train_loop(self):
         i = 0
-        model = MLPRegressor(alpha=0.001, hidden_layer_sizes = (10,), max_iter=50000, 
+        model = MLPRegressor(alpha=0.001, hidden_layer_sizes = (10,), max_iter=5000,
                             activation='logistic', verbose='True', learning_rate='adaptive')
         while True:
             while self.client.get_data(str(i)) == None:
                     time.sleep(2) # be kind to system
             X, y = pickle.loads(self.client.get_data(str(i)))
-            model.partial_fit(X, y)
+            model.fit(X, y)
             print("Training model on data", str(i), flush=True)
             i+=1
 
--- smartsim/__init__.py
+++ smartsim/__init__.py
@@ -6,7 +6,6 @@ __version__ = "0.1.0"
 from .state import State
 from .control import Controller
 from .generation import Generator
-from .process import Processor
 from .simModule import SmartSimModule
 from .smartSimNode import SmartSimNode
 from .client import Client
