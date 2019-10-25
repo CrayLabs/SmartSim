fc38014ffca5880d6518cf7361f4b5068e411596
Audrey Pratt <9011779+astatide@users.noreply.github.com>
2019-10-25 09:50:44 -0700

    Exteneded test_state to assert model params are ok.
    Assert that the supplied dictionaries are the ones set on the model.

--- smartsim/tests/test_state.py
+++ smartsim/tests/test_state.py
@@ -20,9 +20,11 @@ def test_state_api_with_create_target_create_model():
     # a model to each target; if we then fed this state into a generator, the
     # 'atm' state would have _two_ models.
     param_dict = {"25": [20, 25]}
+    model_1_params = {"25": 90}
+    model_2_params = {"25": 125}
     STATE.create_target("atm", params=param_dict)
-    STATE.create_model("add_1", "atm", {"25": 90})
-    STATE.create_model("add_1", params={"25": 125})
+    STATE.create_model("add_1", "atm", model_1_params)
+    STATE.create_model("add_1", params=model_2_params)
 
     # assert that we have created 2 targets.
     assert(len(STATE.targets) == 2)
@@ -42,3 +44,7 @@ def test_state_api_with_create_target_create_model():
     assert(STATE.targets[atm_target].params == param_dict)
     # assert that our params are an empty dictionary for our default target.
     assert(STATE.targets[default_target].params == {})
+
+    # assert that our model params are set appropriately.
+    assert(STATE.targets[atm_target]._models["add_1"].param_dict == model_1_params)
+    assert(STATE.targets[default_target]._models["add_1"].param_dict == model_2_params)
