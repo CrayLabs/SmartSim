00bff1c643d186067a0a5ea75b3394f1d99512f2
Sam Partee <partees21@gmail.com>
2019-11-07 19:14:59 -0600

    Add online inference tests
    The profiling of the online training and inference loops are complete
    with the addition of the online inference tests
    
    also added is a mechanism by which a client can block until a value
    is stored at a specific key. The rate at which the client polls is
    also controllable by the user.

--- smartsim/tests/inference/node.py
+++ smartsim/tests/inference/node.py
@@ -12,13 +12,20 @@ class TrainingNode():
         i = 0
         avg_train_path_time = 0
         while True:
-            data = self.client.get_data(str(i), wait=True)
+            # recieve data from the simulation
+            obj = self.client.get_data(str(i), wait=True)
+
+            # get the time and compare to time sent
             recieve_time = time.time()
-            data = pickle.loads(data)
-            send_time = data[1]
-            avg_train_path_time += abs(recieve_time - send_time)
+            data = pickle.loads(obj)
+            start_time = data[1]
+            avg_train_path_time += abs(recieve_time - start_time)
             print("Recieving data for key", str(i))
             print("Data recieved at: " , str(recieve_time), flush=True)
+
+            # send back immediately to test inference loop overhead
+            self.client.send_big_data(str(i), obj)
+
             i+=1
             if i == 20:
                 break
