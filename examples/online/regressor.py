fde93b2c410f6fe7b40e7c7c19950fe32124b604
Sam Partee <partees21@gmail.com>
2019-11-14 15:51:37 -0600

    Make training example slightly more verbose

--- examples/online/regressor.py
+++ examples/online/regressor.py
@@ -6,20 +6,26 @@ from sklearn.neural_network import MLPRegressor
 from smartsim import Client # The smartsim user client library
 
 class TrainingNode():
-
+    
     def __init__(self):
         self.client = Client()
 
     def train_loop(self):
         i = 0
+        # warm start to use previous weights as initialization
         model = MLPRegressor(alpha=0.001, hidden_layer_sizes=(10,),
-                             max_iter=10000, activation='logistic',
-                             verbose='True', learning_rate='adaptive')
+                             max_iter=5000, activation='logistic',
+                             verbose='True', learning_rate='adaptive',
+                             warm_start=True, tol=-4000)
         while True:
             data = self.client.get_data(str(i), wait=True)
             X, y = pickle.loads(data)
-            model.partial_fit(X, y)
-
+            print("-------------------------------")
+            print("Training on time step ", str(i))
+            print("-------------------------------")
+            time.sleep(1)
+            model.fit(X, y)
+            
             i+=1
             if i == 20:
                 break # end the training session
