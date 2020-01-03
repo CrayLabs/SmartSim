4ed982917750d15a0b14c132cd9878fca9bbc667
Sam Partee <partees21@gmail.com>
2020-01-02 19:06:32 -0600

    fix MOM6 example

--- examples/MOM6/run-double-gyre.py
+++ examples/MOM6/run-double-gyre.py
@@ -4,30 +4,27 @@ from smartsim import Controller, Generator, State
 state = State(experiment="double_gyre")
 
 # Create targets
-quar_deg_params = {"KH": [200, 400, 700],
-                   "KHTH": [200, 400, 700],
+quar_deg_params = {"KH": [200, 400],
+                   "KHTH": [200, 400],
                    "x_resolution": 40,
                    "y_resolution": 20,
                    "months": 1}
-half_deg_params = {"KH": [200, 400, 700],
-                   "KHTH": [200, 400, 700],
+half_deg_params = {"KH": [200, 400],
+                   "KHTH": [200, 400],
                    "x_resolution": 40,
                    "y_resolution": 20,
                    "months": 1}
-state.create_target("quar-deg", params=quar_deg_params)
-state.create_target("half-deg", params=half_deg_params)
-
-
+run_params = {"nodes":2,
+              "executable":"MOM6",
+              "partition": "iv24"}
+state.create_target("quar-deg", params=quar_deg_params, run_settings=run_params)
+state.create_target("half-deg", params=half_deg_params, run_settings=run_params)
 
 # Generate Models
 gen = Generator(state, model_files="./MOM6_base_config")
 gen.generate()
 
-control_dict = {"nodes":2,
-                "executable":"MOM6",
-                "run_command":"srun",
-                "launcher": "slurm",
-                "partition": "iv24"}
-sim = Controller(state, **control_dict)
+sim = Controller(state, launcher="slurm")
 sim.start()
 sim.poll()
+sim.release()
