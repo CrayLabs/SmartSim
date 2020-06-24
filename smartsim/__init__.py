fcce9e936040711378d9510e471d64547e1df34d
Matthew Ellis <mellis@cray.com>
2020-06-24 15:27:11 -0500

    Enforce Python version 3.7
    Squashed commit of the following:
    
    commit e2c9bafccabf776606e81693b7a8d3ef74f10517
    Author: Matthew Ellis <mellis@cray.com>
    Date:   Tue Jun 23 16:40:57 2020 -0700
    
        Enforce python version 3.7.

--- smartsim/__init__.py
+++ smartsim/__init__.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 __version__ = "0.1.0"
 
+import sys
+if sys.version_info < (3,7):
+    sys.exit('Python 3.7 or greater must be used with SmartSim.')
+
 # Main modules
 from .experiment import Experiment
 from .clients import Client
