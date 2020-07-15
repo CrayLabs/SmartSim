4e63c0634e58aa5fac0b164483991585166a1777
Matthew Ellis <mellis@cray.com>
2020-07-15 15:54:03 -0500

    Update LAMMPS tutorial documentation
    The LAMMPS tutorial documentation has
    been updated. Expanded descriptions
    of the C++ client calls in LAMMPS, the
    build process with the C++ client, and
    the analysis script are included herein.
    Scripts have also been updated with additional
    comments and better variable names.
    
    Squashed commit of the following:
    
    commit 86e530bfbf8044f6a85fdc2772d240af5754e60d
    Author: Matthew Ellis <mellis@cray.com>
    Date:   Wed Jul 15 13:44:18 2020 -0700
    
        Update the analysis script documentation in the LAMMPS example.
    
    commit e6ed47002e9c7e56230a35f87ba4a35d4cfc6d86
    Author: Matthew Ellis <mellis@cray.com>
    Date:   Wed Jul 15 12:34:58 2020 -0500
    
        Update run-melt.py for readability.
    
    commit b5cd3f93f9f20ced6977aea3345d1bebcd7d6ba9
    Author: Matthew Ellis <mellis@cray.com>
    Date:   Tue Jul 14 20:31:17 2020 -0700
    
        Update LAMMPS example docs.

--- examples/LAMMPS/melt/run-melt.py
+++ examples/LAMMPS/melt/run-melt.py
@@ -1,33 +1,80 @@
 from smartsim import Experiment
 import os
 
-nodes = 2
+# Define resource variables that we will
+# use to get and manage system resources
+lammps_compute_nodes = 2
+db_compute_nodes = 3
+analysis_compute_nodes = 1
+
+total_compute_nodes = lammps_compute_nodes  + db_compute_nodes + analysis_compute_nodes
+
+# Define the a variable for the max number of processes
+# per compute node that we expect to need
 ppn = 2
 
-experiment = Experiment("lammps_melt_analysis")
-alloc = experiment.get_allocation(nodes=nodes+4, ppn=ppn)
+# Create a SmartSim Experiment using the default
+# Slurm launcher backend
+experiment = Experiment("lammps_experiment")
 
-run_settings = {
-    "nodes": nodes,
+# Fetch a compute resource allocation using SmartSim
+# Experiment API
+alloc = experiment.get_allocation(total_compute_nodes, ppn=ppn)
+
+
+# Define the run settings for the LAMMPS model that will
+# be subsequently created.
+lammps_settings = {
+    "nodes": lammps_compute_nodes,
     "ppn" : ppn,
     "executable": "lmp",
     "exe_args": "-i in.melt",
-    "alloc": alloc
-}
-node_settings = {
-    "nodes": 1,
-    "executable": "python smartsim_node.py",
-    "exe_args": f"--ranks={nodes*ppn} --time=250",
-    "alloc": alloc
-}
+    "alloc": alloc}
 
-m1 = experiment.create_model("lammps_melt", run_settings=run_settings)
+# Define the run settings for the Python analysis script
+# that will be subsequently created
+analysis_settings = {
+    "nodes": analysis_compute_nodes,
+    "executable": "python smartsim_node.py",
+    "exe_args": f"--ranks={lammps_compute_nodes*ppn} --time=250",
+    "alloc": alloc}
+
+# Create the LAMMPS SmartSim model entity with the previously
+# defined run settings
+m1 = experiment.create_model("lammps_melt_model", run_settings=lammps_settings)
+
+# Attach the simulation input file in.melt to the entity so that
+# the input file is copied into the experiment directory when it is created
 m1.attach_generator_files(to_copy=[f"{os.getcwd()}/in.melt"])
-n1 = experiment.create_node("lammps_data_processor",run_settings=node_settings)
+
+# Create the analysis SmartSim node entity with the
+# previously defined run settings
+n1 = experiment.create_node("lammps_data_processor",run_settings=analysis_settings)
+
+# Attach the analysis script to the SmartSim node entity so that
+# the script is copied into the experiment directory when the
+# experiment is generated.
 n1.attach_generator_files(to_copy=[f"{os.getcwd()}/smartsim_node.py"])
+
+# Create the SmartSim orchestrator object and database using the default
+# database cluster setting of three database nodes
 orc = experiment.create_orchestrator_cluster(alloc, overwrite=True)
+
+# Generate the experiment directory structure and copy the files
+# attached to SmartSim entities into that folder structure.
 experiment.generate()
-experiment.start(orchestrator=orc)
+
+# Start the experiment
+experiment.start()
+
+# Poll the status of the SmartSim model and node in a blocking
+# manner until both are completed
 experiment.poll()
+
+# When the model and node are complete, stop the
+# orchestrator with the stop() call which will
+# stop all running jobs when no entities are specified
 experiment.stop()
+
+# Release our system compute allocation
 experiment.release()
