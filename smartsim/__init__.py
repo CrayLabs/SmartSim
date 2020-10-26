17bfbd31af4f96877cb9901aa72f3bb8e4da45ec
Sam Partee <spartee@hpe.com>
2020-10-26 16:30:44 -0700

    Remove MPO implementation
    The MPO implementation that integrates smartsim
    with crayai lacked tests and given our development
    timelines, we won't be able to complete it before
    the second alpha release.

--- smartsim/__init__.py
+++ smartsim/__init__.py
@@ -1,16 +1,17 @@
 
 # -*- coding: utf-8 -*-
 __version__ = "0.1.0"
-
+import os
 import sys
 if sys.version_info < (3,7):
     sys.exit('Python 3.7 or greater must be used with SmartSim.')
 
+## setup smartsim environment
+library_path = os.path.abspath(__file__) + "/../../"
+os.environ["SMARTSIMHOME"] = os.path.realpath(library_path)
+
 # Main API module
 from .experiment import Experiment
-
-# MPO module
-from .mpo import MPO
 
 # Slurm helpers
 from .launcher import slurm
