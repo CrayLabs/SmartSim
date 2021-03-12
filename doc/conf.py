34088d19d70c9c428d82c19dada92b39cabd6af4
Sam Partee <spartee@hpe.com>
2021-03-12 15:55:53 -0800

    Update documentation

--- doc/conf.py
+++ doc/conf.py
@@ -67,5 +67,5 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-
+autoclass_content = 'both'
 add_module_names = False
