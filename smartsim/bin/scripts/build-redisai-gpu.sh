fd33ec25babd62912e2e38efd1a41a62f8fb658b
Sam Partee <spartee@hpe.com>
2021-04-30 15:46:39 -0700

    Add build script for RedisAI GPU

---
+++ smartsim/bin/scripts/build-redisai-gpu.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#Install Redis
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+# Detect OS type
+if [[ "$OSTYPE" == "linux"* ]]; then
+        OS="linux"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "ERROR: RedisAI GPU does not support MacOS"
+else
+        OS="unknown"
+fi
+
+#Install RedisAI
+if [[ -f "$DIR/../../lib/redisai.so" ]]; then
+    echo "RedisAI installed"
+else
+    # check for cudnn includes
+    if [ -z "$CUDNN_INCLUDE_DIR" ]; then
+        echo "ERROR: CUDNN_INCLUDE_DIR is not set"
+        return 1
+    else
+        echo "Found CUDNN_INCLUDE_DIR: $CUDNN_INCLUDE_DIR "
+        if [ -f "$CUDNN_INCLUDE_DIR/cudnn.h" ]; then
+            echo "Found cudnn.h at $CUDNN_INCLUDE_DIR"
+        else
+            echo "ERROR: could not find cudnn.h at $CUDNN_INCLUDE_DIR"
+            return 1
+        fi
+    fi
+
+    # check for cudnn library
+    if [ -z "$CUDNN_LIBRARY" ]; then
+        echo "ERROR: CUDNN_LIBRARY is not set"
+        return 1
+    else
+        echo "Found CUDNN_LIBRARY: $CUDNN_LIBRARY"
+        if [ -f "$CUDNN_LIBRARY/libcudnn.so" ]; then
+            echo "Found libcudnn.so at $CUDNN_LIBRARY"
+        else
+            echo "ERROR: could not find libcudnn.so at $CUDNN_LIBRARY"
+            return 1
+        fi
+    fi
+
+    if [[ ! -d "$DIR/../../.third-party/RedisAI" ]]; then
+        git clone --recursive https://github.com/RedisAI/RedisAI.git --branch v1.2.2 --depth=1 $DIR/../../.third-party/RedisAI
+        echo "RedisAI downloaded"
+    fi
+    echo "Downloading RedisAI GPU ML Runtimes"
+    CC=gcc CXX=g++ WITH_PT=$1 WITH_TF=$2 WITH_TFLITE=$3 WITH_ORT=$4 bash $DIR/../../.third-party/RedisAI/get_deps.sh gpu
+
+    echo "Building RedisAI GPU ML Runtimes"
+    CC=gcc CXX=g++ GPU=1 WITH_PT=$1 WITH_TF=$2 WITH_TFLITE=$3 WITH_ORT=$4 make -C $DIR/../../.third-party/RedisAI/opt clean build
+
+    if [ -f "$DIR/../../.third-party/RedisAI/install-gpu/redisai.so" ]; then
+        cp -r $DIR/../../.third-party/RedisAI/install-gpu/* $DIR/../../lib/
+        echo "Finished installing RedisAI"
+    else
+        echo "ERROR: RedisAI failed to build"
+        return 1
+    fi
+fi
