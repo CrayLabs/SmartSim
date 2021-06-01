c588f677e5118f98c9c86f581869dc4213a66b4c
EricGustin <ericgustin44@gmail.com>
2021-06-01 16:36:18 -0700

    Add tests for Model

---
+++ tests/test_model.py
@@ -0,0 +1,24 @@
+import pytest
+
+from smartsim import Experiment
+from smartsim.error import SSConfigError
+from smartsim.settings import RunSettings
+
+
+def test_register_incoming_entity_preexists():
+    exp = Experiment("experiment", launcher="local")
+    rs = RunSettings("python", exe_args="sleep.py")
+    ensemble = exp.create_ensemble(name="ensemble", replicas=1, run_settings=rs)
+    m = exp.create_model("model", run_settings=rs)
+    m.register_incoming_entity(ensemble[0])
+    assert len(m.incoming_entities) == 1
+    with pytest.raises(SSConfigError):
+        m.register_incoming_entity(ensemble[0])
+
+
+def test_disable_key_prefixing():
+    exp = Experiment("experiment", launcher="local")
+    rs = RunSettings("python", exe_args="sleep.py")
+    m = exp.create_model("model", run_settings=rs)
+    m.disable_key_prefixing()
+    assert m.query_key_prefixing() == False
