b95af20bb9095341bb46c98b8e1011764fb75787
EricGustin <ericgustin44@gmail.com>
2021-06-17 16:15:15 -0700

    Rename to test_entitylist.py

--- tests/test_entitylist.py
+++ tests/test_entitylist.py
@@ -7,19 +7,12 @@ from smartsim.entity import Ensemble, EntityList, Model
 from smartsim.settings import RunSettings
 
 
-def test_entityList_init():
+def test_entity_list_init():
     with pytest.raises(NotImplementedError):
         ent_list = EntityList("list", getcwd(), perm_strat="all_perm")
 
 
-def test_entityList_type():
-    exp = Experiment("name")
-    ens_settings = RunSettings("python")
-    ensemble = exp.create_ensemble("name", replicas=4, run_settings=ens_settings)
-    assert ensemble.type == "Ensemble"
-
-
-def test_entityList_getitem():
+def test_entity_list_getitem():
     """EntityList.__getitem__ is overridden in Ensemble, so we had to pass an instance of Ensemble
     to EntityList.__getitem__ in order to add test coverage to EntityList.__getitem__
     """
@@ -29,7 +22,7 @@ def test_entityList_getitem():
     assert EntityList.__getitem__(ensemble, "name_3") == ensemble[3]
 
 
-def test_entityList_repr():
+def test_entity_list_repr():
     exp = Experiment("name")
     ens_settings = RunSettings("python")
     ensemble = exp.create_ensemble("ens_name", replicas=1, run_settings=ens_settings)
