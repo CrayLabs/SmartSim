59d97ffe1e564a50793cfb146fb171a09997e3c0
Sam Partee <spartee@hpe.com>
2021-12-03 17:38:34 -0600

    Add LSF test to test_create_batch
    Added ``set_queue`` to BsubBatchSettings
    and added a test for LSF

--- tests/test_batch_settings.py
+++ tests/test_batch_settings.py
@@ -1,11 +1,16 @@
+from smartsim.settings import (
+    QsubBatchSettings,
+    SbatchSettings,
+    BsubBatchSettings
+)
 from smartsim.settings.settings import create_batch_settings
-
 
 def test_create_pbs_batch():
     pbs_batch = create_batch_settings(
         "pbs", nodes=1, time="10:00:00", queue="default", account="myproject", ncpus=10
     )  # test that kwargs make it to class init
     args = pbs_batch.format_batch_args()
+    assert(isinstance(pbs_batch, QsubBatchSettings))
     assert args == [
         "-l select=1:ncpus=10",
         "-l place=scatter",
@@ -28,6 +33,7 @@ def test_create_sbatch():
     )  # test that kwargs from
     # pbs doesn't effect slurm (im thinking this will be common)
 
+    assert(isinstance(slurm_batch, SbatchSettings))
     assert slurm_batch.batch_args["partition"] == "default"
     args = slurm_batch.format_batch_args()
     assert args == [
@@ -38,3 +44,18 @@ def test_create_sbatch():
         "--partition=default",
         "--account=myproject",
     ]
+
+
+def test_create_bsub():
+    batch_args = {"core_isolation": None}
+    bsub = create_batch_settings(
+        "lsf",
+        nodes=1,
+        time="10:00:00",
+        account="myproject", # test that account is set
+        queue="default",
+        batch_args=batch_args
+    )
+    assert(isinstance(bsub, BsubBatchSettings))
+    args = bsub.format_batch_args()
+    assert(args == ['-core_isolation', '-nnodes 1', '-q default'])
