d94aed8d7a8136f27c6904ce5c513bce45e37b2f
Eric Gustin <34000337+EricGustin@users.noreply.github.com>
2022-02-05 16:47:08 -0800

    Address Matt's comments

--- tests/on_wlm/test_wlm_orc_config_settings.py
+++ tests/on_wlm/test_wlm_orc_config_settings.py
@@ -1,6 +1,5 @@
 import pytest
 
-from smartsim import Experiment
 from smartsim.error import SmartSimError
 
 # retrieved from pytest fixtures
@@ -15,199 +14,32 @@ except AttributeError:
     pytestmark = pytest.mark.skip(reason="SmartRedis version is < 0.3.0")
 
 
-def test_config_methods_on_wlm(fileutils, wlmutils):
-    """test setting single node orchestrator configurations"""
-    launcher = wlmutils.get_test_launcher()
-
-    exp_name = "test_config_methods_on_wlm"
-    exp = Experiment(exp_name, launcher=launcher)
-    test_dir = fileutils.make_test_dir(exp_name)
-
-    db = wlmutils.get_orchestrator()
-    db.set_path(test_dir)
-
-    exp.start(db)
-    try:
-        db.enable_checkpoints(1)
-        db.set_max_memory("3gb")
-        db.set_eviction_strategy("allkeys-lru")
-        db.set_max_clients(50_000)
-        db.set_max_message_size(2_147_483_648)
-    except:
-        exp.stop(db)
-        assert False
-
-    exp.stop(db)
-
-
-def test_config_methods_on_wlm_cluster(fileutils, wlmutils):
+def test_config_methods_on_wlm_cluster(dbutils, db_cluster):
     """test setting clustered 3-node orchestrator configurations"""
-    launcher = wlmutils.get_test_launcher()
-
-    exp_name = "test_config_methods_on_wlm_cluster"
-    exp = Experiment(exp_name, launcher=launcher)
-    test_dir = fileutils.make_test_dir(exp_name)
-
-    db = wlmutils.get_orchestrator(nodes=3)
-    db.set_path(test_dir)
-
-    exp.start(db)
-    try:
-        db.enable_checkpoints(1)
-        db.set_max_memory("3gb")
-        db.set_eviction_strategy("allkeys-lru")
-        db.set_max_clients(50_000)
-        db.set_max_message_size(2_147_483_648)
-    except:
-        exp.stop(db)
-        assert False
+    configs = dbutils.get_db_configs()
+    for setting, value in configs.items():
+        config_set_method = dbutils.get_config_edit_method(db_cluster, setting)
+        config_set_method(value)
 
 
-def test_config_methods_inactive_on_wlm(fileutils, wlmutils):
-    """Ensure a SmartSimError is raised when trying to
-    set configurations on an inactive database
-    """
-    launcher = wlmutils.get_test_launcher()
-
-    exp_name = "test_config_methods_inactive_on_wlm"
-    exp = Experiment(exp_name, launcher=launcher)
-    test_dir = fileutils.make_test_dir(exp_name)
-
-    db = wlmutils.get_orchestrator()
-    db.set_path(test_dir)
-
-    exp.start(db)
-    exp.stop(db)
-
-    with pytest.raises(SmartSimError):
-        db.enable_checkpoints(1)
-    with pytest.raises(SmartSimError):
-        db.set_max_memory("3gb")
-    with pytest.raises(SmartSimError):
-        db.set_eviction_strategy("allkeys-lru")
-    with pytest.raises(SmartSimError):
-        db.set_max_clients(50_000)
-    with pytest.raises(SmartSimError):
-        db.set_max_message_size(2_147_483_648)
-
-
-def test_config_methods_inactive_on_wlm_cluster(fileutils, wlmutils):
+def test_config_methods_inactive_on_wlm_cluster(wlmutils, dbutils):
     """Ensure a SmartSimError is raised when trying to
     set configurations on an inactive clustered 3-node database
     """
-    launcher = wlmutils.get_test_launcher()
-
-    exp_name = "test_config_methods_inactive_on_wlm_cluster"
-    exp = Experiment(exp_name, launcher=launcher)
-    test_dir = fileutils.make_test_dir(exp_name)
-
     db = wlmutils.get_orchestrator(nodes=3)
-    db.set_path(test_dir)
-
-    exp.start(db)
-    exp.stop(db)
-
-    with pytest.raises(SmartSimError):
-        db.enable_checkpoints(1)
-    with pytest.raises(SmartSimError):
-        db.set_max_memory("3gb")
-    with pytest.raises(SmartSimError):
-        db.set_eviction_strategy("allkeys-lru")
-    with pytest.raises(SmartSimError):
-        db.set_max_clients(50_000)
-    with pytest.raises(SmartSimError):
-        db.set_max_message_size(2_147_483_648)
+    configs = dbutils.get_db_configs()
+    for setting, value in configs.items():
+        config_set_method = dbutils.get_config_edit_method(db, setting)
+        with pytest.raises(SmartSimError):
+            config_set_method(value)
 
 
-def test_bad_db_conf_on_wlm(fileutils, wlmutils):
+def test_bad_db_conf_on_wlm_cluster(dbutils, db_cluster):
     """Ensure SmartSimErrors are raised for all kinds
     of invalid key value pairs
     """
-    launcher = wlmutils.get_test_launcher()
-
-    exp_name = "test_bad_db_conf_on_wlm"
-    exp = Experiment(exp_name, launcher=launcher)
-    test_dir = fileutils.make_test_dir(exp_name)
-
-    db = wlmutils.get_orchestrator()
-    db.set_path(test_dir)
-
-    exp.start(db)
-    bad_configs = {
-        "save": [
-            -1,  # frequency must be positive
-            2.4,  # frequency must be specified in whole seconds
-        ],
-        "maxmemory": [
-            "29GG",  # invalid memory form
-            str(2 ** 65) + "gb",  # memory is too much
-            99,  # memory form must be a string
-            "3.5gb",  # invalid memory form
-        ],
-        "maxclients": [
-            -3,  # number clients must be positive
-            2.9,  # number of clients must be an integer
-            2 ** 65,  # number of clients is too large
-        ],
-        "proto-max-bulk-len": [
-            100,  # max message size can't be smaller than 1mb
-            101.1,  # max message size must be an integer
-            "9.9gb",  # invalid memory form
-        ],
-        "maxmemory-policy": ["invalid-policy"],  # must use a valid maxmemory policy
-        "invalid-parameter": ["99"],  # invalid key - no such configuration exists
-    }
-
+    bad_configs = dbutils.get_bad_db_configs()
     for key, value_list in bad_configs.items():
         for value in value_list:
             with pytest.raises(SmartSimError):
-                db.set_db_conf(key, value)
-
-    exp.stop(db)
-
-
-def test_bad_db_conf_on_wlm_cluster(fileutils, wlmutils):
-    """Ensure SmartSimErrors are raised for all kinds
-    of invalid key value pairs
-    """
-    launcher = wlmutils.get_test_launcher()
-
-    exp_name = "test_bad_db_conf_on_wlm_cluster"
-    exp = Experiment(exp_name, launcher=launcher)
-    test_dir = fileutils.make_test_dir(exp_name)
-
-    db = wlmutils.get_orchestrator(nodes=3)
-    db.set_path(test_dir)
-
-    exp.start(db)
-    bad_configs = {
-        "save": [
-            -1,  # frequency must be positive
-            2.4,  # frequency must be specified in whole seconds
-        ],
-        "maxmemory": [
-            "29GG",  # invalid memory form
-            str(2 ** 65) + "gb",  # memory is too much
-            99,  # memory form must be a string
-            "3.5gb",  # invalid memory form
-        ],
-        "maxclients": [
-            -3,  # number clients must be positive
-            2.9,  # number of clients must be an integer
-            2 ** 65,  # number of clients is too large
-        ],
-        "proto-max-bulk-len": [
-            100,  # max message size can't be smaller than 1mb
-            101.1,  # max message size must be an integer
-            "9.9gb",  # invalid memory form
-        ],
-        "maxmemory-policy": ["invalid-policy"],  # must use a valid maxmemory policy
-        "invalid-parameter": ["99"],  # invalid key - no such configuration exists
-    }
-
-    for key, value_list in bad_configs.items():
-        for value in value_list:
-            with pytest.raises(SmartSimError):
-                db.set_db_conf(key, value)
-
-    exp.stop(db)
+                db_cluster.set_db_conf(key, value)
--- tests/test_orc_config_settings.py
+++ tests/test_orc_config_settings.py
@@ -1,7 +1,5 @@
 import pytest
 
-from smartsim import Experiment
-from smartsim.database import Orchestrator
 from smartsim.error import SmartSimError
 
 try:
@@ -12,93 +10,32 @@ except AttributeError:
     pytestmark = pytest.mark.skip(reason="SmartRedis version is < 0.3.0")
 
 
-def test_config_methods(fileutils):
-    exp_name = "test_config_methods"
-    exp = Experiment(exp_name, launcher="local")
-    test_dir = fileutils.make_test_dir(exp_name)
-
-    db = Orchestrator(db_nodes=1)
-    db.set_path(test_dir)
-    exp.start(db)
-    try:
-        db.enable_checkpoints(1)
-        db.set_max_memory("3gb")
-        db.set_eviction_strategy("allkeys-lru")
-        db.set_max_clients(50_000)
-        db.set_max_message_size(2_147_483_648)
-    except:
-        exp.stop(db)
-        assert False
-
-    exp.stop(db)
+def test_config_methods(dbutils, db):
+    """Ensure all configuration file edit methods in the Orchestrator pass"""
+    configs = dbutils.get_db_configs()
+    for setting, value in configs.items():
+        config_set_method = dbutils.get_config_edit_method(db, setting)
+        config_set_method(value)
 
 
-def test_config_methods_inactive(fileutils):
+def test_config_methods_inactive(wlmutils, dbutils):
     """Ensure a SmartSimError is raised when trying to
     set configurations on an inactive database
     """
-    exp_name = "test_config_methods_inactive"
-    exp = Experiment(exp_name, launcher="local")
-    test_dir = fileutils.make_test_dir(exp_name)
-
-    db = Orchestrator(db_nodes=1)
-    db.set_path(test_dir)
-
-    exp.start(db)
-    exp.stop(db)
-
-    with pytest.raises(SmartSimError):
-        db.enable_checkpoints(1)
-    with pytest.raises(SmartSimError):
-        db.set_max_memory("3gb")
-    with pytest.raises(SmartSimError):
-        db.set_eviction_strategy("allkeys-lru")
-    with pytest.raises(SmartSimError):
-        db.set_max_clients(50_000)
-    with pytest.raises(SmartSimError):
-        db.set_max_message_size(2_147_483_648)
+    db = wlmutils.get_orchestrator()
+    configs = dbutils.get_db_configs()
+    for setting, value in configs.items():
+        config_set_method = dbutils.get_config_edit_method(db, setting)
+        with pytest.raises(SmartSimError):
+            config_set_method(value)
 
 
-def test_bad_db_conf(fileutils):
+def test_bad_db_conf(dbutils, db):
     """Ensure SmartSimErrors are raised for all kinds
     of invalid key value pairs
     """
-    exp_name = "test_bad_db_conf"
-    exp = Experiment(exp_name, launcher="local")
-    test_dir = fileutils.make_test_dir(exp_name)
-
-    db = Orchestrator(db_nodes=1)
-    db.set_path(test_dir)
-
-    exp.start(db)
-    bad_configs = {
-        "save": [
-            -1,  # frequency must be positive
-            2.4,  # frequency must be specified in whole seconds
-        ],
-        "maxmemory": [
-            "29GG",  # invalid memory form
-            str(2 ** 65) + "gb",  # memory is too much
-            99,  # memory form must be a string
-            "3.5gb",  # invalid memory form
-        ],
-        "maxclients": [
-            -3,  # number clients must be positive
-            2.9,  # number of clients must be an integer
-            2 ** 65,  # number of clients is too large
-        ],
-        "proto-max-bulk-len": [
-            100,  # max message size can't be smaller than 1mb
-            101.1,  # max message size must be an integer
-            "9.9gb",  # invalid memory form
-        ],
-        "maxmemory-policy": ["invalid-policy"],  # must use a valid maxmemory policy
-        "invalid-parameter": ["99"],  # invalid key - no such configuration exists
-    }
-
+    bad_configs = dbutils.get_bad_db_configs()
     for key, value_list in bad_configs.items():
         for value in value_list:
             with pytest.raises(SmartSimError):
                 db.set_db_conf(key, value)
-
-    exp.stop(db)
