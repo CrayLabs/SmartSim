92187b944398d48a698b336de58f3551c339b77b
Eric Gustin <34000337+EricGustin@users.noreply.github.com>
2022-03-19 14:38:10 -0700

    Address Sam's comment

--- smartsim/_core/_cli/build.py
+++ smartsim/_core/_cli/build.py
@@ -320,7 +320,7 @@ class Build:
         if os.environ.get("RAI_PATH"):
             if installed_redisai_backends():
                 logger.error(
-                    f"Backends are already built and loaded at {CONFIG.redisai}. There is no need to build."
+                    f"There is no need to build. Backends are already built and specified in the environment at 'RAI_PATH': {CONFIG.redisai}"
                 )
             else:
                 logger.error(
