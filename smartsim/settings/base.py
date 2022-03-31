1dcf81a88b9b39e3fea37da73c0eedfee8e2be8f
Ben Albrecht <benjamin.albrecht@hpe.com>
2022-03-31 13:58:33 -0400

    Update update_env doc with a note about inheriting env
    ---
    Signed-off-by: Ben Albrecht <ben-albrecht@users.noreply.github.com>

--- smartsim/settings/base.py
+++ smartsim/settings/base.py
@@ -359,6 +359,12 @@ class RunSettings:
     def update_env(self, env_vars):
         """Update the job environment variables
 
+        To fully inherit the current user environment, add the
+        workload-manager-specific flag to the launch command through the
+        :meth:`add_exe_args` method. For example, ``--export=ALL`` for slurm,
+        or ``-V`` for PBS/aprun.
+
+
         :param env_vars: environment variables to update or add
         :type env_vars: dict[str, Union[str, int, float, bool]]
         :raises TypeError: if env_vars values cannot be coerced to strings
