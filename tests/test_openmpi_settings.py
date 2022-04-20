82209d03387c32b61c3e49d17bcd73a7f36dea96
Matthew Drozt <drozt@hpe.com>
2022-04-20 16:58:42 -0400

    Detect OpenMPI Use (#186)
    Adds new run settings classes for using different
    OpenMPI executables (mpiexec, orterun). Because
    the executables share runtime arguments with mpirun
    much the functionality has be transfered to a base class
    that all OpenMPI run settings inherit from. Adds tests
    and docs updates to reflect changes.
    
    [ committed by @MattToast  ]
    [ reviewed by @Spartee @al-rigazzi ]

--- tests/test_openmpi_settings.py
+++ tests/test_openmpi_settings.py
@@ -1,11 +1,150 @@
+from asyncio.log import logger
+from unicodedata import name
 import pytest
 
-from smartsim.settings import MpirunSettings
+import logging
+import os
+import os.path as osp
+import stat
+import sys
+
+from smartsim.settings.mpirunSettings import (
+    _OpenMPISettings,
+    MpirunSettings,
+    MpiexecSettings,
+    OrterunSettings,
+)
 from smartsim.error import SSUnsupportedError
 
 
-def test_mpirun_settings():
-    settings = MpirunSettings("python")
+@pytest.mark.parametrize(
+    "OpenMPISettings", [MpirunSettings, MpiexecSettings, OrterunSettings]
+)
+def test_not_instanced_if_not_found(OpenMPISettings):
+    old_path = os.getenv("PATH")
+    try:
+        os.environ["PATH"] = ""
+        with pytest.raises(FileNotFoundError):
+            OpenMPISettings(sys.executable)
+    finally:
+        os.environ["PATH"] = old_path
+
+
+@pytest.mark.parametrize(
+    "OpenMPISettings,stubs_path,stub_exe",
+    [
+        pytest.param(
+            MpirunSettings,
+            osp.join("mpi_impl_stubs", "openmpi4"),
+            "mpirun",
+            id="OpenMPI4-mpirun",
+        ),
+        pytest.param(
+            MpiexecSettings,
+            osp.join("mpi_impl_stubs", "openmpi4"),
+            "mpiexec",
+            id="OpenMPI4-mpiexec",
+        ),
+        pytest.param(
+            OrterunSettings,
+            osp.join("mpi_impl_stubs", "openmpi4"),
+            "orterun",
+            id="OpenMPI4-orterun",
+        ),
+    ],
+)
+def test_expected_openmpi_instance_without_warning(
+    OpenMPISettings, stubs_path, stub_exe, fileutils, caplog
+):
+    from smartsim.settings.mpirunSettings import logger
+
+    old_path = os.environ.get("PATH")
+    old_prop = logger.propagate
+    logger.propagate = True
+
+    try:
+        stubs_path = fileutils.get_test_dir_path(stubs_path)
+        stub_exe = osp.join(stubs_path, stub_exe)
+        st = os.stat(stub_exe)
+        if not st.st_mode & stat.S_IEXEC:
+            os.chmod(stub_exe, st.st_mode | stat.S_IEXEC)
+
+        os.environ["PATH"] = stubs_path
+        with caplog.at_level(logging.WARNING):
+            caplog.clear()
+            OpenMPISettings(sys.executable)
+            for rec in caplog.records:
+                if logging.WARNING <= rec.levelno:
+                    pytest.fail(
+                        (
+                            "Unexepected log message when instancing valid "
+                            "OpenMPI settings"
+                        )
+                    )
+    finally:
+        os.environ["PATH"] = old_path
+        logger.propagate = old_prop
+
+
+@pytest.mark.parametrize(
+    "OpenMPISettings,stubs_path,stub_exe",
+    [
+        pytest.param(
+            MpirunSettings,
+            osp.join("mpi_impl_stubs", "intel2019"),
+            "mpirun",
+            id="OpenMPI4-mpirun",
+        ),
+        pytest.param(
+            MpiexecSettings,
+            osp.join("mpi_impl_stubs", "intel2019"),
+            "mpiexec",
+            id="OpenMPI4-mpiexec",
+        ),
+        pytest.param(
+            OrterunSettings,
+            osp.join("mpi_impl_stubs", "intel2019"),
+            "orterun",
+            id="OpenMPI4-orterun",
+        ),
+    ],
+)
+def test_warning_if_not_expected_openmpi(
+    OpenMPISettings, stubs_path, stub_exe, fileutils, caplog
+):
+    from smartsim.settings.mpirunSettings import logger
+
+    old_path = os.environ.get("PATH")
+    old_prop = logger.propagate
+    logger.propagate = True
+
+    try:
+        stubs_path = fileutils.get_test_dir_path(stubs_path)
+        stub_exe = osp.join(stubs_path, stub_exe)
+        st = os.stat(stub_exe)
+        if not st.st_mode & stat.S_IEXEC:
+            os.chmod(stub_exe, st.st_mode | stat.S_IEXEC)
+
+        os.environ["PATH"] = stubs_path
+        with caplog.at_level(logging.WARNING):
+            caplog.clear()
+            OpenMPISettings(sys.executable)
+
+            for rec in caplog.records:
+                if (
+                    logging.WARNING <= rec.levelno < logging.ERROR
+                    and "Non-OpenMPI implementation" in rec.msg
+                ):
+                    break
+            else:
+                pytest.fail("No Non-OpenMPI warning given to user")
+    finally:
+        os.environ["PATH"] = old_path
+        logger.propagate = old_prop
+
+
+def test_openmpi_base_settings():
+    settings = _OpenMPISettings("python")
     settings.set_cpus_per_task(1)
     settings.set_tasks(2)
     settings.set_hostlist(["node005", "node006"])
@@ -14,12 +153,12 @@ def test_mpirun_settings():
     assert formatted == result
 
 
-def test_mpirun_args():
+def test_openmpi_base_args():
     run_args = {
         "map-by": "ppr:1:node",
         "np": 1,
     }
-    settings = MpirunSettings("python", run_args=run_args)
+    settings = _OpenMPISettings("python", run_args=run_args)
     formatted = settings.format_run_args()
     result = ["--map-by", "ppr:1:node", "--np", "1"]
     assert formatted == result
@@ -29,25 +168,25 @@ def test_mpirun_args():
     assert formatted == result
 
 
-def test_mpirun_add_mpmd():
-    settings = MpirunSettings("python")
-    settings_2 = MpirunSettings("python")
+def test_openmpi_add_mpmd():
+    settings = _OpenMPISettings("python")
+    settings_2 = _OpenMPISettings("python")
     settings.make_mpmd(settings_2)
     assert len(settings.mpmd) > 0
     assert settings.mpmd[0] == settings_2
 
 
 def test_catch_colo_mpmd():
-    settings = MpirunSettings("python")
+    settings = _OpenMPISettings("python")
     settings.colocated_db_settings = {"port": 6379, "cpus": 1}
-    settings_2 = MpirunSettings("python")
+    settings_2 = _OpenMPISettings("python")
     with pytest.raises(SSUnsupportedError):
         settings.make_mpmd(settings_2)
 
 
 def test_format_env():
     env_vars = {"OMP_NUM_THREADS": 20, "LOGGING": "verbose"}
-    settings = MpirunSettings("python", env_vars=env_vars)
+    settings = _OpenMPISettings("python", env_vars=env_vars)
     settings.update_env({"OMP_NUM_THREADS": 10})
     formatted = settings.format_env_vars()
     result = [
@@ -60,26 +199,26 @@ def test_format_env():
 
 
 def test_mpirun_hostlist_errors():
-    settings = MpirunSettings("python")
+    settings = _OpenMPISettings("python")
     with pytest.raises(TypeError):
         settings.set_hostlist(4)
 
 
 def test_mpirun_hostlist_errors_1():
-    settings = MpirunSettings("python")
+    settings = _OpenMPISettings("python")
     with pytest.raises(TypeError):
         settings.set_hostlist([444])
 
 
 @pytest.mark.parametrize("reserved_arg", ["wd", "wdir"])
 def test_no_set_reserved_args(reserved_arg):
-    srun = MpirunSettings("python")
+    srun = _OpenMPISettings("python")
     srun.set(reserved_arg)
     assert reserved_arg not in srun.run_args
 
 
 def test_set_cpus_per_task():
-    rs = MpirunSettings("python")
+    rs = _OpenMPISettings("python")
     rs.set_cpus_per_task(6)
     assert rs.run_args["cpus-per-proc"] == 6
 
@@ -88,7 +227,7 @@ def test_set_cpus_per_task():
 
 
 def test_set_tasks_per_node():
-    rs = MpirunSettings("python")
+    rs = _OpenMPISettings("python")
     rs.set_tasks_per_node(6)
     assert rs.run_args["npernode"] == 6
 
@@ -97,7 +236,7 @@ def test_set_tasks_per_node():
 
 
 def test_set_tasks():
-    rs = MpirunSettings("python")
+    rs = _OpenMPISettings("python")
     rs.set_tasks(6)
     assert rs.run_args["n"] == 6
 
@@ -106,7 +245,7 @@ def test_set_tasks():
 
 
 def test_set_hostlist():
-    rs = MpirunSettings("python")
+    rs = _OpenMPISettings("python")
     rs.set_hostlist(["host_A", "host_B"])
     assert rs.run_args["host"] == "host_A,host_B"
 
@@ -118,7 +257,7 @@ def test_set_hostlist():
 
 
 def test_set_hostlist_from_file():
-    rs = MpirunSettings("python")
+    rs = _OpenMPISettings("python")
     rs.set_hostlist_from_file("./path/to/hostfile")
     assert rs.run_args["hostfile"] == "./path/to/hostfile"
 
@@ -127,7 +266,7 @@ def test_set_hostlist_from_file():
 
 
 def test_set_verbose():
-    rs = MpirunSettings("python")
+    rs = _OpenMPISettings("python")
     rs.set_verbose_launch(True)
     assert "verbose" in rs.run_args
 
@@ -139,7 +278,7 @@ def test_set_verbose():
 
 
 def test_quiet_launch():
-    rs = MpirunSettings("python")
+    rs = _OpenMPISettings("python")
     rs.set_quiet_launch(True)
     assert "quiet" in rs.run_args
 
@@ -151,7 +290,7 @@ def test_quiet_launch():
 
 
 def test_set_broadcast():
-    rs = MpirunSettings("python")
+    rs = _OpenMPISettings("python")
     rs.set_broadcast()
     assert "preload-binary" in rs.run_args
 
@@ -160,7 +299,7 @@ def test_set_broadcast():
 
 
 def test_set_time():
-    rs = MpirunSettings("python")
+    rs = _OpenMPISettings("python")
     rs.set_time(minutes=1, seconds=12)
     assert rs.run_args["timeout"] == "72"
 
