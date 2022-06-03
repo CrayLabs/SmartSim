4299e7df9410bb8151c42030bdac16f050411b39
Al Rigazzi <al.rigazzi@hpe.com>
2022-06-03 22:46:08 +0200

    Avoid checking for alloc_specs if it is not defined (#202)
    This PR fixes some issues when `alloc_specs` is not defined (or on systems where it cannot be defined). 
    It also addresses a problem with protobuf versions, which made it impossible to build SmartSim.
    
    [ committed by @al-rigazzi ]
    [ reviewed by @ben-albrecht ]

--- setup.py
+++ setup.py
@@ -141,7 +141,7 @@ deps = [
     "redis==3.5.3",
     "tqdm>=4.50.2",
     "filelock>=3.4.2",
-    "click==8.0.2"
+    "protobuf==3.20"
 ]
 
 # Add SmartRedis at specific version
@@ -154,6 +154,7 @@ extras_require = {
         "pylint>=2.6.0",
         "pytest>=6.0.0",
         "pytest-cov>=2.10.1"
+        "click==8.0.2",
     ],
     # see smartsim/_core/_install/buildenv.py for more details
     "ml": versions.ml_extras_required(),
