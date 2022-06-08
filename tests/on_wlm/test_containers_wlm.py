9e8f4a6c228bce055bcfd2e866d434aa3f40cf01
Ben Albrecht <balbrecht@cray.com>
2022-06-08 11:54:46 -0500

    Introduce container-WLM test suite

---
+++ tests/on_wlm/test_containers_wlm.py
@@ -0,0 +1,63 @@
+import pytest
+from shutil import which
+
+from smartsim import Experiment, status
+from smartsim.entity import Ensemble
+from smartsim.settings.containers import Singularity
+
+"""Test SmartRedis container integration on a supercomputer with a WLM.
+   This test will also run locally if singularity is installed."""
+
+# Check if singularity is available as command line tool
+singularity_exists = which('singularity') is not None
+
+@pytest.mark.skipif(not singularity_exists, reason="Test needs singularity to run")
+def test_singularity_exchange(fileutils, wlmutils):
+    """Run two processes, each process puts a tensor on
+    the DB, then accesses the other process's tensor.
+    Finally, the tensor is used to run a model.
+    """
+
+    test_dir = fileutils.make_test_dir()
+    exp = Experiment(
+        "smartredis_ensemble_exchange", exp_path=test_dir, launcher="auto"
+    )
+
+    # create and start a database
+    orc = exp.create_database()
+    exp.generate(orc)
+    exp.start(orc, block=False)
+
+    container = Singularity('docker://benalbrecht10/smartsim-testing:latest')
+    rs = exp.create_run_settings("python3", "producer.py --exchange", container=container)
+    rs.set_tasks(1)
+    params = {"mult": [1, -10]}
+    ensemble = Ensemble(
+        name="producer",
+        params=params,
+        run_settings=rs,
+        perm_strat="step",
+    )
+
+    ensemble.register_incoming_entity(ensemble["producer_0"])
+    ensemble.register_incoming_entity(ensemble["producer_1"])
+
+    config = fileutils.get_test_conf_path("smartredis")
+    ensemble.attach_generator_files(to_copy=[config])
+
+    exp.generate(ensemble)
+
+    # start the models
+    exp.start(ensemble, summary=False)
+
+    # get and confirm statuses
+    statuses = exp.get_status(ensemble)
+    if not all([stat == status.STATUS_COMPLETED for stat in statuses]):
+        exp.stop(orc)
+        assert False  # client ensemble failed
+
+    # stop the orchestrator
+    exp.stop(orc)
+
+    print(exp.summary())
+
