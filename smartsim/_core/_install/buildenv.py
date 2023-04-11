076ba24a1953d319ecd876b965b6ce1ab2f75b1a
Matt Drozt <drozt@hpe.com>
2023-04-11 14:14:58 -0700

    bump sr to 0.4.0

--- smartsim/_core/_install/buildenv.py
+++ smartsim/_core/_install/buildenv.py
@@ -245,7 +245,7 @@ class Versioner:
 
     # Versions
     SMARTSIM = Version_(get_env("SMARTSIM_VERSION", "0.4.2"))
-    SMARTREDIS = Version_(get_env("SMARTREDIS_VERSION", "0.3.1"))
+    SMARTREDIS = Version_(get_env("SMARTREDIS_VERSION", "0.4.0"))
     SMARTSIM_SUFFIX = get_env("SMARTSIM_SUFFIX", "")
 
     # Redis
