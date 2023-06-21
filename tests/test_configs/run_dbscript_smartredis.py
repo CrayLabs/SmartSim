2083fdc4b800af4ea57f84aa596181904a610e32
Matt Ellis <matthew.ellis@hpe.com>
2023-06-21 14:52:04 -0500

    Remove debug statements.

--- tests/test_configs/run_dbscript_smartredis.py
+++ tests/test_configs/run_dbscript_smartredis.py
@@ -39,22 +39,16 @@ def main():
     assert client.poll_model("test_script1", 500, 30)
     client.run_script("test_script1", "average", ["test_array"], ["test_output"])
     returned = client.get_tensor("test_output")
-    print(f"Return value from test_script_1: {returned}")
-    print(f"Expected value from test_script1: {np.mean(array)}")
     assert returned == approx(np.mean(array))
 
     assert client.poll_model("test_script2", 500, 30)
     client.run_script("test_script2", "negate", ["test_array"], ["test_output"])
     returned = client.get_tensor("test_output")
-    print(f"Return value from test_script_2: {returned}")
-    print(f"Expected value from test_script2: {-array}")
     assert returned == approx(-array)
 
     if client.model_exists("test_func"):
         client.run_script("test_func", "timestwo", ["test_array"], ["test_output"])
         returned = client.get_tensor("test_output")
-        print(f"Return value from test_func: {returned}")
-        print(f"Expected value from test_func: {2 * array}")
         assert returned == approx(2 * array)
 
     print(f"Test worked!")
