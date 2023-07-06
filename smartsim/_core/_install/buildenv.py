81f4485b076996835c56a719a91ebfe68b51c583
Andrew Shao <aeshao@gmail.com>
2023-07-06 12:13:29 -0500

    Bump the SmartRedis version to 0.4.1

--- smartsim/_core/_install/buildenv.py
+++ smartsim/_core/_install/buildenv.py
@@ -245,7 +245,7 @@ class Versioner:
 
     # Versions
     SMARTSIM = Version_(get_env("SMARTSIM_VERSION", "0.4.2"))
-    SMARTREDIS = Version_(get_env("SMARTREDIS_VERSION", "0.4.0"))
+    SMARTREDIS = Version_(get_env("SMARTREDIS_VERSION", "0.4.1"))
     SMARTSIM_SUFFIX = get_env("SMARTSIM_SUFFIX", "")
 
     # Redis
