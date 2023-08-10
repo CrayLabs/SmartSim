f43e7e006b9d57f4735b148a5c0b8c1ac1bf55f1
Christopher McBride <christopher.mcbride@gmail.com>
2023-08-10 12:37:29 -0400

    Fix colocated db preparation bug when using `JsrunSettings`

--- smartsim/entity/model.py
+++ smartsim/entity/model.py
@@ -294,8 +294,8 @@ class Model(SmartSimEntity):
                 "Models colocated with databases cannot be run as a mpmd workload"
             )
 
-        if hasattr(self.run_settings, "prep_colocated_db"):
-            self.run_settings.prep_colocated_db(common_options["cpus"])
+        if hasattr(self.run_settings, "_prep_colocated_db"):
+            self.run_settings._prep_colocated_db(common_options["cpus"])
 
         if "limit_app_cpus" in kwargs:
             raise SSUnsupportedError(
