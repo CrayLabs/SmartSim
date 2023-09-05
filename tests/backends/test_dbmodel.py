af9e864dc91a12da01cf48728e88f1c5f59dfdd9
Al Rigazzi <al.rigazzi@hpe.com>
2023-09-05 16:35:57 +0000

    Update coverage file

--- tests/backends/test_dbmodel.py
+++ tests/backends/test_dbmodel.py
@@ -804,7 +804,7 @@ def test_colocated_db_model_errors(fileutils, wlmutils, mlutils):
 @pytest.mark.skipif(not should_run_tf, reason="Test needs TensorFlow to run")
 def test_inconsistent_params_db_model():
     """Test error when devices_per_node parameter>1 when devices is set to CPU in DBModel"""
-    
+
     # Create and save ML model to filesystem
     model, inputs, outputs = create_tf_cnn()
     with pytest.raises(SSUnsupportedError) as ex:
@@ -819,6 +819,6 @@ def test_inconsistent_params_db_model():
             outputs=outputs,
         )
     assert (
-            ex.value.args[0] 
+            ex.value.args[0]
             == "Cannot set devices_per_node>1 if CPU is specified under devices"
         )
