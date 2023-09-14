c43e91fcdaf110fca0c318f9dcfee9b25c20d734
Christopher McBride <christopher.mcbride@gmail.com>
2023-09-14 19:05:06 -0400

    use pathlike hint instead of int to enable passing path

--- smartsim/_core/launcher/taskManager.py
+++ smartsim/_core/launcher/taskManager.py
@@ -26,6 +26,7 @@
 
 from __future__ import annotations
 
+import os
 import psutil
 import time
 import typing as t
@@ -105,8 +106,8 @@ class TaskManager:
         cmd_list: t.List[str],
         cwd: str,
         env: t.Optional[t.Dict[str, str]] = None,
-        out: int = PIPE,
-        err: int = PIPE,
+        out: t.Union[int, os.PathLike] = PIPE,
+        err: t.Union[int, os.PathLike] = PIPE,
     ) -> str:
         """Start a task managed by the TaskManager
 
