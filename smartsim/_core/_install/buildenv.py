5bdf6eb1da06055008de06e4dbde3fd0b95fbdab
Julia Putko <putko@Julias-MacBook-Pro.local>
2023-10-11 14:24:46 -0700

    pointing to develop smartredis and changelog

--- smartsim/_core/_install/buildenv.py
+++ smartsim/_core/_install/buildenv.py
@@ -274,7 +274,7 @@ class Versioner:
 
     # Versions
     SMARTSIM = Version_(get_env("SMARTSIM_VERSION", "0.5.1"))
-    SMARTREDIS = Version_(get_env("SMARTREDIS_VERSION", "0.4.1"))
+    SMARTREDIS = Version_(get_env("SMARTREDIS_VERSION", "0.4.2"))
     SMARTSIM_SUFFIX = get_env("SMARTSIM_SUFFIX", "")
 
     # Redis
