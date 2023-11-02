79d5afffece08bdba2b46f5085269dac507023fb
Julia Putko <putko@Julias-MacBook-Pro.local>
2023-11-02 12:40:27 -0700

    update to Experiment.create_database() doc

--- smartsim/experiment.py
+++ smartsim/experiment.py
@@ -746,6 +746,9 @@ class Experiment:
         :type queue: str, optional
         :param single_cmd: run all shards with one (MPMD) command, defaults to True
         :type single_cmd: bool, optional
+        :param db_identifier: an identifier to distinguish this orchestrator in 
+            multiple-database experiments, defaults to "orchestrator"
+        :type db_identifier: str, optional
         :raises SmartSimError: if detection of launcher or of run command fails
         :raises SmartSimError: if user indicated an incompatible run command
             for the launcher
