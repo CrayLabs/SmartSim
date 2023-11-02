e90ec59f74b1138721d0c1ce27f5a1d3c8e8c41a
Al Rigazzi <al.rigazzi@hpe.com>
2023-11-02 16:53:44 +0100

    Add vis torchscript refine

--- tutorials/online_analysis/lattice/probe.py
+++ tutorials/online_analysis/lattice/probe.py
@@ -1,5 +1,3 @@
-# import torch
-# import torch.nn.functional as F
 def multi_unsqueeze(tensor, axes: List[int]):
     for axis in axes:
         tensor = torch.unsqueeze(tensor, axis)
@@ -11,10 +9,8 @@ def probe_points(ux, uy, probe_x, probe_y, cylinder):
     uy[cylinder>0] = 0.0
     ux = multi_unsqueeze(ux, [0, 0])
     uy = multi_unsqueeze(uy, [0, 0])
-    # Explicit Euler scheme
     probe_xy = multi_unsqueeze(torch.stack((probe_x/200 - 1, probe_y/50 - 1), 2), [0])
     u_probex = torch.grid_sampler(ux.double(), probe_xy.double(), 0, 0, False).squeeze()
     u_probey = torch.grid_sampler(uy.double(), probe_xy.double(), 0, 0, False).squeeze()
 
-    #return probe_xy.squeeze()
     return torch.stack((u_probex, u_probey), 2)
