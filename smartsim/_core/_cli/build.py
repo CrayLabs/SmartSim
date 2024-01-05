b7fe003c93d424ce4614936955110accae20e567
Andrew Shao <aeshao@gmail.com>
2024-01-05 17:49:45 -0600

    Fix missing comma

--- smartsim/_core/_cli/build.py
+++ smartsim/_core/_cli/build.py
@@ -261,11 +261,11 @@ def check_py_torch_version(versions: Versioner, device: _TDeviceStr = "cpu") -> 
             "Torch version not found in python environment. "
             "Attempting to install via `pip`"
         )
-        wheel_device = device if device == "cpu" else device_suffix
+        wheel_device = device if device == "cpu" else device_suffix.replace("+","")
         pip(
             "install",
             "--extra-index-url",
-            f"https://download.pytorch.org/whl/{wheel_device}"
+            f"https://download.pytorch.org/whl/{wheel_device}",
             *(f"{package}=={version}" for package, version in torch_deps.items()),
         )
     elif missing or conflicts:
