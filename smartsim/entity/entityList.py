ec1c91cc84fb32c2fa969de515dec0bae2b2714a
Christopher McBride <christopher.mcbride@gmail.com>
2024-01-30 17:02:58 -0500

    remove extraneous call to super init

--- smartsim/entity/entityList.py
+++ smartsim/entity/entityList.py
@@ -74,7 +74,6 @@ class EntitySequence(t.Generic[_T_co]):
         # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
         self._initialize_entities(**kwargs)
-        super().__init__()
 
     def _initialize_entities(self, **kwargs: t.Any) -> None:
         """Initialize the SmartSimEntity objects in the container"""
