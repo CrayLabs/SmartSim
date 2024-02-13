d1306338c650aac76e5f0bab4b48650764ef75e1
Julia Putko <81587103+juliaputko@users.noreply.github.com>
2024-02-12 20:26:50 -0800

    Experiment Preview (#447)
    Add a Preview method on Experiment in SmartSim. This method allows users to view the experiment name, path, and launcher of their created experiment before the launch of the experiment. This commit is a subcomponent of the entire Preview method. 
    
    [ committed by @juliaputko  ]
    [ reviewed by @ankona @al-rigazzi  ]

---
+++ tests/test_preview.py
@@ -0,0 +1,107 @@
+# BSD 2-Clause License
+#
+# Copyright (c) 2021-2023, Hewlett Packard Enterprise
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import pathlib
+
+import pytest
+
+from smartsim import Experiment
+from smartsim._core import previewrenderer
+from smartsim.error.errors import PreviewFormatError
+
+
+def test_experiment_preview(test_dir, wlmutils):
+    """Test correct preview output items for Experiment preview"""
+    # Prepare entities
+    test_launcher = wlmutils.get_test_launcher()
+    exp_name = "test_prefix"
+    exp = Experiment(exp_name, exp_path=test_dir, launcher=test_launcher)
+
+    # Execute method for template rendering
+    output = previewrenderer.render(exp)
+
+    # Evaluate output
+    summary_lines = output.split("\n")
+    summary_lines = [item.replace("\t", "").strip() for item in summary_lines[-3:]]
+    assert 3 == len(summary_lines)
+    summary_dict = dict(row.split(": ") for row in summary_lines)
+    assert set(["Experiment", "Experiment Path", "Launcher"]).issubset(summary_dict)
+
+
+def test_experiment_preview_properties(test_dir, wlmutils):
+    """Test correct preview output properties for Experiment preview"""
+    # Prepare entities
+    test_launcher = wlmutils.get_test_launcher()
+    exp_name = "test_experiment_preview_properties"
+    exp = Experiment(exp_name, exp_path=test_dir, launcher=test_launcher)
+
+    # Execute method for template rendering
+    output = previewrenderer.render(exp)
+
+    # Evaluate output
+    summary_lines = output.split("\n")
+    summary_lines = [item.replace("\t", "").strip() for item in summary_lines[-3:]]
+    assert 3 == len(summary_lines)
+    summary_dict = dict(row.split(": ") for row in summary_lines)
+    assert exp.name == summary_dict["Experiment"]
+    assert exp.exp_path == summary_dict["Experiment Path"]
+    assert exp.launcher == summary_dict["Launcher"]
+
+
+def test_preview_to_file(test_dir, wlmutils, fileutils):
+    """
+    Test that if an output_filename is given, a file
+    is rendered for Experiment preview"
+    """
+    # Prepare entities
+    test_launcher = wlmutils.get_test_launcher()
+    exp_name = "test_preview_output_filename"
+    exp = Experiment(exp_name, exp_path=test_dir, launcher=test_launcher)
+    filename = "test_preview_output_filename.txt"
+    path = pathlib.Path(test_dir) / filename
+    # Execute preview method
+    exp.preview(output_format="plain_text", output_filename=str(path))
+
+    # Evaluate output
+    assert path.exists()
+    assert path.is_file()
+
+
+def test_output_format_error():
+    """
+    Test error when invalid ouput format is given.
+    """
+    # Prepare entities
+    exp_name = "test_output_format"
+    exp = Experiment(exp_name)
+
+    # Execute preview method
+    with pytest.raises(PreviewFormatError) as ex:
+        exp.preview(output_format="hello")
+    assert (
+        "The only valid output format currently available is plain_text"
+        in ex.value.args[0]
+    )
