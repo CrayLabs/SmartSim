3fbaa857605450daa9ac7be97fc7765b331d1811
Julia Putko <putko@osprey.us.cray.com>
2024-02-14 17:06:07 -0600

    remove test, corrected spacing in templates

--- smartsim/_core/control/previewrenderer.py
+++ smartsim/_core/control/previewrenderer.py
@@ -92,6 +92,7 @@ def render(
         config=CONFIG,
         verbosity_level=verbosity_level,
     )
+    print(rendered_preview)
     return rendered_preview
 
 
--- tests/test_preview.py
+++ tests/test_preview.py
@@ -201,48 +201,6 @@ def test_preview_active_infrastructure(wlmutils, test_dir, choose_host):
     exp.stop(orc)
 
 
-def test_active_infrastructure_preview_output_format_html(
-    test_dir, wlmutils, choose_host
-):
-    """Test that an html file is rendered for active infrastructure preview"""
-    # Prepare entities
-    test_launcher = wlmutils.get_test_launcher()
-    test_interface = wlmutils.get_test_interface()
-    test_port = wlmutils.get_test_port()
-    exp_name = "test_orchestrator_preview_output_format_html"
-    exp = Experiment(exp_name, exp_path=test_dir, launcher=test_launcher)
-
-    orc = exp.create_database(
-        port=test_port,
-        interface=test_interface,
-        hosts=choose_host(wlmutils),
-        db_identifier="orc_1",
-    )
-    exp.start(orc)
-    assert orc.is_active() == True
-
-    orc2 = exp.create_database(
-        port=test_port,
-        interface=test_interface,
-        hosts=choose_host(wlmutils),
-        db_identifier="orc_2",
-    )
-
-    assert orc2.is_active() == False
-
-    filename = "test_active_infrastructure_preview_output_format_html.html"
-    path = pathlib.Path(test_dir) / filename
-
-    # Execute preview method
-    exp.preview(orc, orc2, output_format="html", output_filename=str(path))
-
-    # Evaluate output
-    assert path.exists()
-    assert path.is_file()
-
-    exp.stop(orc)
-
-
 def test_model_preview(test_dir, wlmutils):
     """
     Test correct preview output fields for Model preview
