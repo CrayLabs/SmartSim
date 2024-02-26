f1db02569fb9f880d00b5091610bcf7564828604
Matthew Ellis <matthew.ellis@hpe.com>
2024-02-26 12:52:51 -0800

    Change the generic t.any typing on start, stop, generate, and get_status.

--- smartsim/experiment.py
+++ smartsim/experiment.py
@@ -36,7 +36,7 @@ from smartsim.error.errors import SSUnsupportedError
 from ._core import Controller, Generator, Manifest
 from ._core.utils import init_default
 from .database import Orchestrator
-from .entity import Ensemble, Model, SmartSimEntity
+from .entity import Ensemble, EntitySequence, Model, SmartSimEntity
 from .error import SmartSimError
 from .log import ctx_exp_path, get_logger, method_contextualizer
 from .settings import Container, base, settings
@@ -149,7 +149,7 @@ class Experiment:
     @_contextualize
     def start(
         self,
-        *args: t.Any,
+        *args: t.Union[SmartSimEntity, EntitySequence[SmartSimEntity]],
         block: bool = True,
         summary: bool = False,
         kill_on_interrupt: bool = True,
@@ -221,7 +221,9 @@ class Experiment:
             raise
 
     @_contextualize
-    def stop(self, *args: t.Any) -> None:
+    def stop(
+        self, *args: t.Union[SmartSimEntity, EntitySequence[SmartSimEntity]]
+    ) -> None:
         """Stop specific instances launched by this ``Experiment``
 
         Instances of ``Model``, ``Ensemble`` and ``Orchestrator``
@@ -260,7 +262,7 @@ class Experiment:
     @_contextualize
     def generate(
         self,
-        *args: t.Any,
+        *args: t.Union[SmartSimEntity, EntitySequence[SmartSimEntity]],
         tag: t.Optional[str] = None,
         overwrite: bool = False,
         verbose: bool = False,
@@ -364,7 +366,9 @@ class Experiment:
             raise
 
     @_contextualize
-    def get_status(self, *args: t.Any) -> t.List[str]:
+    def get_status(
+        self, *args: t.Union[SmartSimEntity, EntitySequence[SmartSimEntity]]
+    ) -> t.List[str]:
         """Query the status of launched instances
 
         Return a smartsim.status string representing
