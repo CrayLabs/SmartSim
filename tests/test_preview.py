66c31cc8797ec2f8b8ee221c51eb67c78ec5dfba
Julia Putko <putko@osprey.us.cray.com>
2024-02-26 16:14:33 -0600

    added batch arguments to ensemble, added tests for wlm previews, fixed typos  in tests

--- tests/test_preview.py
+++ tests/test_preview.py
@@ -53,6 +53,12 @@ def choose_host():
     return _choose_host
 
 
+def add_batch_resources(wlmutils, batch_settings):
+    if isinstance(batch_settings, QsubBatchSettings):
+        for key, value in wlmutils.get_batch_resources().items():
+            batch_settings.set_resource(key, value)
+
+
 def test_experiment_preview(test_dir, wlmutils):
     """Test correct preview output fields for Experiment preview"""
     # Prepare entities
@@ -355,13 +361,10 @@ def test_model_preview(test_dir, wlmutils):
     # Execute preview method
     rendered_preview = previewrenderer.render(exp, preview_manifest)
 
-    print(rendered_preview)
-
     # Evaluate output
     assert "Model Name" in rendered_preview
     assert "Executable" in rendered_preview
     assert "Executable Arguments" in rendered_preview
-    assert "Batch Launch" in rendered_preview
     assert "Model Parameters" in rendered_preview
 
 
@@ -400,7 +403,6 @@ def test_model_preview_properties(test_dir, wlmutils):
     assert hw_name in rendered_preview
     assert hw_param1 in rendered_preview
     assert hw_rs in rendered_preview
-    assert "False" in rendered_preview
     assert "port" in rendered_preview
     assert "password" in rendered_preview
     assert str(hw_port) in rendered_preview
@@ -464,7 +466,6 @@ def test_model_key_prefixing(test_dir, wlmutils):
     """
     Test preview for enabling key prefixing for a Model
     """
-
     # Prepare entities
     exp_name = "test_model_key_prefixing"
     test_launcher = wlmutils.get_test_launcher()
@@ -531,7 +532,7 @@ def test_preview_models_and_ensembles(test_dir, wlmutils):
     """
     Test preview of separate model entity and ensemble entity
     """
-    exp_name = "test-model-and-ensemble"
+    exp_name = "test-preview-model-and-ensemble"
     test_dir = pathlib.Path(test_dir) / exp_name
     test_dir.mkdir(parents=True)
     test_launcher = wlmutils.get_test_launcher()
@@ -563,11 +564,13 @@ def test_preview_models_and_ensembles(test_dir, wlmutils):
 
 def test_ensemble_preview_client_configuration(test_dir, wlmutils):
     """
-    Test client configuration and key prefixing in Ensemble preview
+    Test preview of client configuration and key prefixing in Ensemble preview
     """
     # Prepare entities
     test_launcher = wlmutils.get_test_launcher()
-    exp = Experiment("key_prefix_test", exp_path=test_dir, launcher=test_launcher)
+    exp = Experiment(
+        "test-preview-ensemble-clientconfig", exp_path=test_dir, launcher=test_launcher
+    )
     # Create Orchestrator
     db = exp.create_database(port=6780, interface="lo")
     exp.generate(db, overwrite=True)
@@ -598,12 +601,14 @@ def test_ensemble_preview_client_configuration(test_dir, wlmutils):
 
 def test_ensemble_preview_client_configuration_multidb(test_dir, wlmutils):
     """
-    Test client configuration and key prefixing in Ensemble preview
-    with multiple databses
+    Test preview of client configuration and key prefixing in Ensemble preview
+    with multiple databases
     """
     # Prepare entities
     test_launcher = wlmutils.get_test_launcher()
-    exp = Experiment("key_prefix_test", exp_path=test_dir, launcher=test_launcher)
+    exp = Experiment(
+        "test-preview-multidb-clinet-config", exp_path=test_dir, launcher=test_launcher
+    )
     # Create Orchestrator
     db1_dbid = "db_1"
     db1 = exp.create_database(port=6780, interface="lo", db_identifier=db1_dbid)
@@ -647,7 +652,9 @@ def test_ensemble_preview_attached_files(fileutils, test_dir, wlmutils):
     """
     # Prepare entities
     test_launcher = wlmutils.get_test_launcher()
-    exp = Experiment("attached-files-test", exp_path=test_dir, launcher=test_launcher)
+    exp = Experiment(
+        "test-preview-attached-files", exp_path=test_dir, launcher=test_launcher
+    )
     ensemble = exp.create_ensemble(
         "dir_test", replicas=1, run_settings=RunSettings("python", exe_args="sleep.py")
     )
@@ -693,11 +700,11 @@ def test_ensemble_preview_attached_files(fileutils, test_dir, wlmutils):
 
 
 def test_preview_colocated_db_model_ensemble(fileutils, test_dir, wlmutils, mlutils):
-    """Test preview of DBModel on colocated ensembles, first adding the DBModel to the
-    ensemble, then colocating DB.
+    """
+    Test preview of DBModel on colocated ensembles
     """
 
-    exp_name = "test-colocated-db-model-ensemble-reordered"
+    exp_name = "test-preview-colocated-db-model-ensemble"
     test_launcher = wlmutils.get_test_launcher()
     test_interface = wlmutils.get_test_interface()
     test_port = wlmutils.get_test_port()
@@ -812,7 +819,7 @@ def test_preview_colocated_db_script_ensemble(fileutils, test_dir, wlmutils, mlu
     Test preview of DB Scripts on colocated DB from ensemble
     """
 
-    exp_name = "test-colocated-db-script"
+    exp_name = "test-preview-colocated-db-script"
 
     test_launcher = wlmutils.get_test_launcher()
     test_interface = wlmutils.get_test_interface()
@@ -921,11 +928,11 @@ def test_preview_wlm_run_commands_cluster_orc_model(
     test_dir, coloutils, fileutils, wlmutils
 ):
     """
-    Test preview of srun run command and run aruguments on a
-    clustered 3-node orchestrator and model
+    Test preview of wlm run command and run aruguments on a
+    orchestrator and model
     """
 
-    exp_name = "test_preview_launch_cluster_orc"
+    exp_name = "test-preview-orc-model"
     launcher = wlmutils.get_test_launcher()
     test_port = wlmutils.get_test_port()
     test_script = fileutils.get_test_conf_path("smartredis/multidbid.py")
@@ -958,7 +965,12 @@ def test_preview_wlm_run_commands_cluster_orc_model(
 
     # Execute preview method
     output = previewrenderer.render(exp, preview_manifest)
-    # assert
+
+    # Evaluate output
+    assert "Run Command" in output
+    assert "Run Arguments" in output
+    assert "ntasks" in output
+    assert "nodes" in output
 
 
 @pytest.mark.skipif(
@@ -966,7 +978,10 @@ def test_preview_wlm_run_commands_cluster_orc_model(
     reason="Not testing WLM integrations",
 )
 def test_preview_model_on_wlm(fileutils, test_dir, wlmutils):
-    exp_name = "test-base-settings-model-launch"
+    """
+    Test preview of wlm run command and run aruguments for a model
+    """
+    exp_name = "test-preview-model-wlm"
     exp = Experiment(exp_name, launcher=wlmutils.get_test_launcher(), exp_path=test_dir)
 
     script = fileutils.get_test_conf_path("sleep.py")
@@ -975,8 +990,6 @@ def test_preview_model_on_wlm(fileutils, test_dir, wlmutils):
     M1 = exp.create_model("m1", path=test_dir, run_settings=settings1)
     M2 = exp.create_model("m2", path=test_dir, run_settings=settings2)
 
-    # launch models twice to show that they can also be restarted
-    # exp.preview(M1, M2)
     preview_manifest = Manifest(M1, M2)
 
     # Execute preview method
@@ -989,12 +1002,10 @@ def test_preview_model_on_wlm(fileutils, test_dir, wlmutils):
     assert "time" in output
 
 
-def add_batch_resources(wlmutils, batch_settings):
-    if isinstance(batch_settings, QsubBatchSettings):
-        for key, value in wlmutils.get_batch_resources().items():
-            batch_settings.set_resource(key, value)
-
-
+@pytest.mark.skipif(
+    pytest.test_launcher not in pytest.wlm_options,
+    reason="Not testing WLM integrations",
+)
 def test_preview_batch_model(fileutils, test_dir, wlmutils):
     """Test the preview of a model with batch settings"""
 
@@ -1013,6 +1024,42 @@ def test_preview_batch_model(fileutils, test_dir, wlmutils):
     model.set_path(test_dir)
 
     preview_manifest = Manifest(model)
+
+    # Execute preview method
+    output = previewrenderer.render(exp, preview_manifest)
+
+    assert "Batch Launch: True" in output
+    assert "Batch Commands" in output
+    assert "Batch Arguments" in output
+    assert "nodes" in output
+    assert "time" in output
+
+
+@pytest.mark.skipif(
+    pytest.test_launcher not in pytest.wlm_options,
+    reason="Not testing WLM integrations",
+)
+def test_preview_batch_ensemble(fileutils, test_dir, wlmutils):
+    """Test preview of a batch ensemble"""
+
+    exp_name = "test-preview-batch-ensemble"
+    exp = Experiment(exp_name, launcher=wlmutils.get_test_launcher(), exp_path=test_dir)
+
+    script = fileutils.get_test_conf_path("sleep.py")
+    settings = wlmutils.get_run_settings("python", f"{script} --time=5")
+    M1 = exp.create_model("m1", path=test_dir, run_settings=settings)
+    M2 = exp.create_model("m2", path=test_dir, run_settings=settings)
+
+    batch = exp.create_batch_settings(nodes=1, time="00:01:00")
+    add_batch_resources(wlmutils, batch)
+
+    batch.set_account(wlmutils.get_test_account())
+    ensemble = exp.create_ensemble("batch-ens", batch_settings=batch)
+    ensemble.add_model(M1)
+    ensemble.add_model(M2)
+    ensemble.set_path(test_dir)
+
+    preview_manifest = Manifest(ensemble)
 
     # Execute preview method
     output = previewrenderer.render(exp, preview_manifest)
