eda5fcb2d58deb00fbbc96077b541067f96e20ab
Julia Putko <putko@osprey.us.cray.com>
2024-02-27 11:22:52 -0600

    black fix

--- smartsim/_core/control/previewrenderer.py
+++ smartsim/_core/control/previewrenderer.py
@@ -160,9 +160,7 @@ def _check_verbosity_level(
     """
     if not isinstance(verbosity_level, Verbosity):
 
-        logger.warning(
-            f"'{verbosity_level}' is an unsupported verbosity level.\
- Setting verbosity to: {Verbosity.INFO}"
-        )
+        logger.warning(f"'{verbosity_level}' is an unsupported verbosity level.\
+ Setting verbosity to: {Verbosity.INFO}")
         return Verbosity.INFO
     return verbosity_level
