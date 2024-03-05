95a2f8f04c05141f889ccf4aefb2e60679d2bcec
Amanda Richardson <amanda.richardson@hpe.com>
2024-03-05 11:50:27 -0800

    delete changelog exclude

--- doc/conf.py
+++ doc/conf.py
@@ -66,7 +66,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', "**.ipynb_checkpoints", 'changelog.rst']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', "**.ipynb_checkpoints"]
 
 breathe_projects = {
         "c_client":"../smartredis/doc/c_client/xml",
