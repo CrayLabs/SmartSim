07de12f8a26da993d2d208b901ca52490be069a4
Amanda Richardson <amanda162000@gmail.com>
2024-03-21 23:28:28 -0500

    test

--- smartsim/_core/_install/builder.py
+++ smartsim/_core/_install/builder.py
@@ -516,11 +516,11 @@ class RedisAIBuilder(Builder):
         fetchable_deps: t.List[_RAIBuildDependency] = [_DLPackRepository("v0.5_RAI")]
         if self.fetch_torch:
             pt_dep = _choose_pt_variant(os_)
-            fetchable_deps.append(pt_dep(arch, Device(device), "2.0.1"))
+            fetchable_deps.append(pt_dep(arch, device, "2.0.1"))
         if self.fetch_tf:
-            fetchable_deps.append(_TFArchive(os_, arch, Device(device), "2.13.1"))
+            fetchable_deps.append(_TFArchive(os_, arch, device, "2.13.1"))
         if self.fetch_onnx:
-            fetchable_deps.append(_ORTArchive(os_, Device(device), "1.16.3"))
+            fetchable_deps.append(_ORTArchive(os_, device, "1.16.3"))
 
         return tuple(fetchable_deps)
 
