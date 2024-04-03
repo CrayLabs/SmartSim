98e5e8edf00b5ef121cd0671a5f5f4d4f527f1ab
Amanda Richardson <amanda162000@gmail.com>
2024-04-03 17:42:55 -0500

    updating

--- doc/conf.py
+++ doc/conf.py
@@ -57,8 +57,6 @@ extensions = [
     'sphinx.ext.mathjax',
 ]
 
-sphinx_tabs_valid_builders = ['linkcheck']
-
 autodoc_mock_imports = ["smartredis.smartredisPy"]
 suppress_warnings = ['autosectionlabel']
 
