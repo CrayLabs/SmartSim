9ce12ffa16e0afed43260c7a6f07987ab5e642f7
Andrew Shao <Andrew.Shao@hpe.com>
2024-04-03 19:20:28 -0700

    dust lint

--- smartsim/_core/_install/builder.py
+++ smartsim/_core/_install/builder.py
@@ -1073,7 +1073,7 @@ def _modify_source_files(
     files: t.Union[_PathLike, t.Iterable[_PathLike]], regex: str, replacement: str
 ) -> None:
     compiled_regex = re.compile(regex)
-    with fileinput.input(files=files, encoding="utf-8", inplace=True) as f:
-        for line in f:
+    with fileinput.input(files=files, encoding="utf-8", inplace=True) as handles:
+        for line in handles:
             line = compiled_regex.sub(replacement, line)
             print(line, end="")
