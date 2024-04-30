5264db61351429e43f7e5c76135b0a45418d2558
Amanda Richardson <amanda.richardson@hpe.com>
2024-04-29 23:27:31 -0700

    ignore link

--- doc/conf.py
+++ doc/conf.py
@@ -78,6 +78,10 @@ source_suffix = {
     '.md': 'markdown',
 }
 
+linkcheck_ignore = [
+    'Redis::set_model_multigpu',
+]
+
 # The path to the MathJax.js file that Sphinx will use to render math expressions
 mathjax_path = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'
 
