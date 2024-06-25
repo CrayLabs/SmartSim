53b05292f4541879466c20df88aa7432d3ca31d8
Julia Putko <putko@osprey.us.cray.com>
2024-06-24 19:07:08 -0500

    addressing amanda comments, bug fixes

--- smartsim/_core/generation/commandgenerator.py
+++ smartsim/_core/generation/commandgenerator.py
@@ -24,71 +24,128 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# TODO: rename and make all the descriptions accurate and good
-# including the params and stuff
+import typing as t
+from os import path
+
+
+def move_op(to_move: str, dest_file: str):
+    """Write a python script to moves a file
+
+    :param to_move: Path to a source file to be copied
+    :param dest_file: Path to a file to copy the contents from the source file into
+    :return: string of python code to perform operation that can be executed
+    """
+    return f"import shutil;shutil.move('{to_move}','{dest_file}')"
+
 
 def delete_op(to_delete: str):
-  """Produce string of a script that deletes a file when executed.
+    """Write a python script that deletes a file when executed.
 
-  :param to_delete: path to the file to be deleted
-  :return: string to be shipped as a command that deletes the given file
+    :param to_delete: Path to the file to be deleted
+    :return: string of python code to perform operation that can be executed
     """
-  return f"import os;os.remove('{to_delete}')"
+
+    if path.isfile(to_delete):
+        return rf"""
+import os
+os.remove('{to_delete}')
+    """
+    else:
+        raise FileNotFoundError(f"{to_delete} is not a valid path")
 
 
-def copy_op(source_file: str, dest_file: str):
-   """return command that can be popened"""
-   return f"import shutil;shutil.copyfile('{source_file}', '{dest_file}')"
+def copy_op(to_copy: str, entity_path: str):
+    """
+    Write a python script to copy the entity files and directories attached
+    to this entity into an entity directory
+
+    :param to_copy: Path to directory, or path to file to copy into an entity directory
+    :param entity_path: Path to a directory of an entity
+    :return: string of python code to perform operation that can be executed
+    """
+
+    if not path.exists(entity_path):
+        raise FileNotFoundError(f"{entity_path} is not a valid path")
+    elif not path.exists(to_copy):
+        raise FileNotFoundError(f"{to_copy} is not a valid path")
+
+    else:
+
+        return rf"""import shutil
+from distutils import dir_util
+from os import path
+dst_path = path.join('{entity_path}', path.basename('{to_copy}'))
 
 
-def symlink_op(dest_file: str, source_file: str):
+if path.isdir('{to_copy}'):
+  dir_util.copy_tree('{to_copy}', '{entity_path}')
+else:
+  shutil.copyfile('{to_copy}', dst_path)
+"""
+
+
+def symlink_op(to_link: str, entity_path: str):
+    """
+    Write a python script to make the to_link path a symlink pointing to the entity path.
+
+    :param to_link: path to link
+    :param entity_path:
+    :return: string of python code to perform operation that can be executed
+    """
+    return rf"""import os
+from os import path
+dest_path = path.join('{entity_path}', path.basename('{to_link}'))
+
+os.symlink('{to_link}', dest_path)
   """
-  Make the source path a symlink pointing to the destination path.
-
-  :param source_file:
-  :param dest_file:
-  :return:
-  """
-  return f"import os;os.symlink(str('{source_file}'), str('{dest_file}'))"
-
-def move_op(source_file, dest_file):
-    """Execute a script that moves a file"""
-    return f"import shutil;shutil.move('{source_file}','{dest_file}')"
 
 
-def configure_op(file_path, param_dict):
-  """configure file with params
-  TODO: clean this up"""
+def configure_op(
+    to_configure: str, params: t.Dict[str, str], tag_delimiter: t.Optional[str] = None
+):
+    """Write a python script to set, search and replace the tagged parameters for the configure operation
+    within tagged files attached to an entity.
 
-  return fr"""
-import re
+    :param to configure:
+    :param params: A dict of parameter names and values set for the file
+    :tag_delimiter: tag for the configure operation to search for, defaults to semi-colon e.g. ";"
+    :return: string of python code to perform operation that can be executed
+    """
+
+    return rf"""import re
 import collections
 
-edited = []
-used_params = {{}}
-files_to_tags = {{}}
-params = {param_dict}
-regex = "(;.+;)"
-tag = ";"
-
 def _get_prev_value(tagged_line: str) -> str:
-    split_tag = tagged_line.split(tag)
+    split_tag = tagged_line.split(tag_delimiter)
     return split_tag[1]
 
 def _is_ensemble_spec(
     tagged_line: str, model_params: dict
   ) -> bool:
-      split_tag = tagged_line.split(tag)
+      split_tag = tagged_line.split(tag_delimiter)
       prev_val = split_tag[1]
       if prev_val in model_params.keys():
           return True
       return False
 
-with open('{file_path}','r+', encoding='utf-8') as file_stream:
+edited = []
+used_params = {{}}
+params = {params}
+
+tag_delimiter = '{tag_delimiter}'
+
+if tag_delimiter == 'False':
+  tag_delimiter = ';'
+
+regex = "".join(("(", tag_delimiter, ".+", tag_delimiter, ")"))
+
+# Set the lines to iterate over
+with open('{to_configure}','r+', encoding='utf-8') as file_stream:
   lines = file_stream.readlines()
 
 unused_tags = collections.defaultdict(list)
 
+# read lines in file
 for i, line in enumerate(lines, 1):
     while search := re.search(regex, line):
         tagged_line = search.group(0)
@@ -98,18 +155,18 @@ for i, line in enumerate(lines, 1):
             line = re.sub(regex, new_val, line, 1)
             used_params[previous_value] = new_val
 
-        # if a tag is found but is not in this model's configurations
+        # if a tag_delimiter is found but is not in this model's configurations
         # put in placeholder value
         else:
-            tag = tagged_line.split(tag)[1]
-            unused_tags[tag].append(i)
+            tag_delimiter_ = tagged_line.split(tag_delimiter)[1]
+            unused_tags[tag_delimiter_].append(i)
             line = re.sub(regex, previous_value, line)
             break
     edited.append(line)
 
 lines = edited
 
-with open('{file_path}', "w+", encoding="utf-8") as file_stream:
+with open('{to_configure}', "w+", encoding="utf-8") as file_stream:
     for line in lines:
         file_stream.write(line)
-"""
+  """
--- tests/test_command_generation.py
+++ tests/test_command_generation.py
@@ -25,99 +25,199 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import filecmp
-from glob import glob
 import os
 import pathlib
 import subprocess
 import sys
 import typing as t
+from distutils import dir_util
+from glob import glob
 from os import path as osp
 
-from distutils import dir_util
+import pytest
 
 from smartsim._core.generation import commandgenerator
 
 test_path = os.path.dirname(os.path.abspath(__file__))
+test_output_root = os.path.join(test_path, "tests", "test_output")
 
-@staticmethod
-def make_test_file(
-    file_name: str, file_dir: str, file_content: t.Optional[str] = None
-) -> str:
-    """Create a dummy file in the test output directory.
 
-    :param file_name: name of file to create, e.g. "file.txt"
-    :param file_dir: path
-    :return: String path to test output file
+def test_symlink_entity():
     """
-    file_path = os.path.join(file_dir, file_name)
-    os.makedirs(file_dir)
-    with open(file_path, "w+", encoding="utf-8") as dummy_file:
-        if not file_content:
-            dummy_file.write("dummy\n")
-        else:
-            dummy_file.write(file_content)
-    return file_path
-
-
-def test_symlink():
+    Test the execution of the python script created by symlink_op
     """
-    Test symlink. Make path a symlink pointing to the given path
-    """
-    # Prep test directory
-    test_output_root = os.path.join(test_path, "tests", "test_output")
-    target = pathlib.Path(test_output_root) / "sym_target_dir" / "target"
+
     source = pathlib.Path(test_output_root) / "sym_source"
+    os.mkdir(source)
 
-    # Build the command
-    cmd = commandgenerator.symlink_op(source, target)
+    # entity_path to be the dest dir
+    entity_path = os.path.join(test_output_root, "entity_name")
+    os.mkdir(entity_path)
 
-    # execute the command
+    target = pathlib.Path(test_output_root) / "entity_name" / "sym_source"
+
+    # # Build the command
+    cmd = commandgenerator.symlink_op(source, entity_path)
+    # # execute the command
     subprocess.run([sys.executable, "-c", cmd])
 
-    # Assert the two files are the same file
-    assert source.is_symlink()
-    assert os.readlink(source) == str(target)
+    # # Assert the two files are the same file
+    assert target.is_symlink()
+    assert os.readlink(target) == str(source)
 
     # Clean up the test directory
-    os.unlink(source)
+    os.unlink(target)
+    os.rmdir(pathlib.Path(test_output_root) / "entity_name")
+    os.rmdir(pathlib.Path(test_output_root) / "sym_source")
 
 
-def test_copy():
-    """Copy the content of the source file to the destination file."""
+def test_copy_op_file():
+    """Test the execution of the python script created by copy_op
+    Test the operation to copy the content of the source file to the destination path
+    with an empty file of the same name already in the directory"""
 
-    test_output_root = os.path.join(test_path, "tests", "test_output")
+    to_copy = os.path.join(test_output_root, "to_copy")
+    os.mkdir(to_copy)
+
+    source_file = pathlib.Path(to_copy) / "copy_file.txt"
+    with open(source_file, "w+", encoding="utf-8") as dummy_file:
+        dummy_file.write("dummy")
+
+    entity_path = os.path.join(test_output_root, "entity_name")
+    os.mkdir(entity_path)
+
+    dest_file = os.path.join(test_output_root, "entity_name", "copy_file.txt")
+    with open(dest_file, "w+", encoding="utf-8") as dummy_file:
+        dummy_file.write("")
+
+    # Execute copy
+    cmd = commandgenerator.copy_op(source_file, entity_path)
+    subprocess.run([sys.executable, "-c", cmd])
+
+    # clean up
+    os.remove(pathlib.Path(to_copy) / "copy_file.txt")
+    os.rmdir(pathlib.Path(test_output_root) / "to_copy")
+
+    os.remove(pathlib.Path(entity_path) / "copy_file.txt")
+    os.rmdir(pathlib.Path(test_output_root) / "entity_name")
+
+
+def test_copy_op_dirs():
+    """Test the execution of the python script created by copy_op
+    Test the oeprations that copies an entire directory tree source to a new location destination
+    """
+
+    to_copy = os.path.join(test_output_root, "to_copy")
+    os.mkdir(to_copy)
+
+    # write some test files in the dir
+    source_file = pathlib.Path(to_copy) / "copy_file.txt"
+    with open(source_file, "w+", encoding="utf-8") as dummy_file:
+        dummy_file.write("dummy1")
+
+    source_file_2 = pathlib.Path(to_copy) / "copy_file_2.txt"
+    with open(source_file_2, "w+", encoding="utf-8") as dummy_file:
+        dummy_file.write("dummy2")
+
+    # entity_path to be the dest dir
+    entity_path = os.path.join(test_output_root, "entity_name")
+    os.mkdir(entity_path)
+    # copy those files?
+
+    cmd = commandgenerator.copy_op(to_copy, entity_path)
+    subprocess.run([sys.executable, "-c", cmd])
+
+    # Clean up
+    os.remove(pathlib.Path(to_copy) / "copy_file.txt")
+    os.remove(pathlib.Path(to_copy) / "copy_file_2.txt")
+    os.rmdir(pathlib.Path(test_output_root) / "to_copy")
+    os.remove(pathlib.Path(entity_path) / "copy_file.txt")
+    os.remove(pathlib.Path(entity_path) / "copy_file_2.txt")
+    os.rmdir(pathlib.Path(test_output_root) / "entity_name")
+
+
+def test_copy_op(fileutils):
+    """Test the execution of the python script created by copy_op
+    Test the operation to copy the content of the source file to the destination file.
+    """
 
     # make a test file with some contents
-    source_file = make_test_file(
-        "source.txt", pathlib.Path(test_output_root) / "source", "dummy"
+    to_copy_file = fileutils.make_test_file(
+        "to_copy.txt", pathlib.Path(test_output_root) / "to_copy", "dummy"
     )
-    dest_file = make_test_file("dest.txt", pathlib.Path(test_output_root) / "dest", "")
+
+    entity_path = os.path.join(test_output_root, "entity_name")
+
+    os.mkdir(entity_path)
 
     # assert that source file exists, has correct contents
-    assert osp.exists(source_file)
-    with open(source_file, "r", encoding="utf-8") as dummy_file:
+    assert osp.exists(to_copy_file)
+    with open(to_copy_file, "r", encoding="utf-8") as dummy_file:
         assert dummy_file.read() == "dummy"
 
-    cmd = commandgenerator.copy_op(source_file, dest_file)
+    cmd = commandgenerator.copy_op(to_copy_file, entity_path)
     subprocess.run([sys.executable, "-c", cmd])
 
-    with open(dest_file, "r", encoding="utf-8") as dummy_file:
-        assert dummy_file.read() == "dummy"
+    entity_file = os.path.join(test_output_root, "entity_name", "to_copy.txt")
+    # asser that the entity_path now has the source file with the correct contents
+    with open(entity_file, "r", encoding="utf-8") as dummy_file:
+        assert "dummy" in dummy_file.read()
 
     # Clean up the test directory
-    os.remove(source_file)
-    os.remove(dest_file)
-    os.rmdir(pathlib.Path(test_output_root) / "source")
-    os.rmdir(pathlib.Path(test_output_root) / "dest")
+    os.remove(to_copy_file)
+    os.remove(entity_file)
+    os.rmdir(pathlib.Path(test_output_root) / "to_copy")
+    os.rmdir(pathlib.Path(test_output_root) / "entity_name")
 
 
-def test_move():
-    """Test to move command execution"""
-    test_output_root = os.path.join(test_path, "tests", "test_output")
+def test_copy_op_bad_source_file():
+    """Test the execution of the python script created by copy_op
+    Test that a FileNotFoundError is raised when there is a bad source file
+    """
+
+    to_copy = os.path.join(test_output_root, "to_copy")
+    os.mkdir(to_copy)
+    entity_path = os.path.join(test_output_root, "entity_name")
+    os.mkdir(entity_path)
+
+    # Execute copy
+    with pytest.raises(FileNotFoundError) as ex:
+        commandgenerator.copy_op("/not/a/real/path", entity_path)
+    assert "is not a valid path" in ex.value.args[0]
+    # clean up
+    os.rmdir(pathlib.Path(test_output_root) / "to_copy")
+    os.rmdir(pathlib.Path(test_output_root) / "entity_name")
+
+
+def test_copy_op_bad_dest_path():
+    """Test the execution of the python script created by copy_op.
+    Test that a FileNotFoundError is raised when there is a bad destination file."""
+
+    to_copy = os.path.join(test_output_root, "to_copy")
+    os.mkdir(to_copy)
+
+    source_file = pathlib.Path(to_copy) / "copy_file.txt"
+    entity_path = os.path.join(test_output_root, "entity_name")
+    os.mkdir(entity_path)
+
+    with pytest.raises(FileNotFoundError) as ex:
+        commandgenerator.copy_op(source_file, "/not/a/real/path")
+    assert "is not a valid path" in ex.value.args[0]
+
+    # clean up
+    os.rmdir(pathlib.Path(test_output_root) / "to_copy")
+    os.rmdir(pathlib.Path(test_output_root) / "entity_name")
+
+
+def test_move_op():
+    """Test the execution of the python script created by move_op.
+    Test the operation to move a file"""
+
     source_dir = os.path.join(test_output_root, "from_here")
     os.mkdir(source_dir)
     dest_dir = os.path.join(test_output_root, "to_here")
     os.mkdir(dest_dir)
+
     dest_file = os.path.join(test_output_root, "to_here", "to_here.txt")
     source_file = pathlib.Path(source_dir) / "app_move.txt"
 
@@ -128,11 +228,10 @@ def test_move():
     with open(source_file, "r", encoding="utf-8") as dummy_file:
         assert dummy_file.read() == "dummy"
 
-
     cmd = commandgenerator.move_op(source_file, dest_file)
     subprocess.run([sys.executable, "-c", cmd])
 
-    # assert that the move was successful
+    # Assert that the move was successful
     assert not osp.exists(source_file)
     assert osp.exists(dest_file)
     with open(dest_file, "r", encoding="utf-8") as dummy_file:
@@ -144,9 +243,9 @@ def test_move():
     os.rmdir(dest_dir)
 
 
-def test_delete():
-    """Test python inline command to delete a file"""
-    test_output_root = os.path.join(test_path, "tests", "test_output")
+def test_delete_op():
+    """Test the execution of the python script created by delete_op.
+    Test the operation to delete a file"""
 
     # Make a test file with dummy text
     to_del = pathlib.Path(test_output_root) / "app_del.txt"
@@ -164,8 +263,58 @@ def test_delete():
     assert not osp.exists(to_del)
 
 
-def test_configure(test_dir, fileutils):
-    """ test configure param operations """
+def test_delete_op_bad_path():
+    """Test the execution of the python script created by delete_op.
+    Test that FileNotFoundError is raised when a bad path is given to the
+    soperation to delete a file"""
+
+    test_output_root = os.path.join(test_path, "tests", "test_output")
+    to_del = pathlib.Path(test_output_root) / "not_real.txt"
+
+    with pytest.raises(FileNotFoundError) as ex:
+        commandgenerator.delete_op(to_del)
+    assert "is not a valid path" in ex.value.args[0]
+
+
+def test_configure_op(test_dir, fileutils):
+    """Test the execution of the python script created by configure_op.
+    Test configure param operations with a tag parameter given"""
+
+    # the param dict for configure operations
+    param_dict = {
+        "5": 10,  # MOM_input
+        "FIRST": "SECOND",  # example_input.i
+        "17": 20,  # in.airebo
+        "65": "70",  # in.atm
+        "placeholder": "group leftupper region",  # in.crack
+        "1200": "120",  # input.nml
+    }
+    tag = ";"
+    # retreive tagged files
+    conf_path = fileutils.get_test_conf_path(osp.join("tagged_tests", "marked/"))
+    # retrieve files to compare after test
+    correct_path = fileutils.get_test_conf_path(osp.join("tagged_tests", "correct/"))
+
+    # copy files to test directory
+    dir_util.copy_tree(conf_path, test_dir)
+    assert osp.isdir(test_dir)
+
+    tagged_files = sorted(glob(test_dir + "/*"))
+    correct_files = sorted(glob(correct_path + "*"))
+
+    # Run configure op on test files
+    for tagged_file in tagged_files:
+        cmd = commandgenerator.configure_op(tagged_file, param_dict, tag)
+        subprocess.run([sys.executable, "-c", cmd])
+
+    # check that files and correct files are the same
+    for written, correct in zip(tagged_files, correct_files):
+        assert filecmp.cmp(written, correct)
+
+
+def test_configure_op_no_tag(test_dir, fileutils):
+    """Test the execution of the python script created by configure_op.
+    Test configure param operations with no tag parameter given"""
 
     # the param dict for configure operations
     param_dict = {
@@ -190,7 +339,7 @@ def test_configure(test_dir, fileutils):
 
     # Run configure op on test files
     for tagged_file in tagged_files:
-        cmd = commandgenerator.configure_op(tagged_file, param_dict)
+        cmd = commandgenerator.configure_op(tagged_file, param_dict, False)
         subprocess.run([sys.executable, "-c", cmd])
 
     # check that files and correct files are the same
