f6efd72e522aec27b901b3ecd93c8d7975babd58
Andrew Shao <Andrew.Shao@hpe.com>
2024-07-16 15:17:29 -0700

    Remove SmartRedis version from Versioner

--- setup.py
+++ setup.py
@@ -212,7 +212,7 @@ setup(
         "pyzmq>=25.1.2",
         "pygithub>=2.3.0",
         "numpy<2",
-	"smartredis<=0.5.3,
+	    "smartredis<=0.5.3",
     ],
     cmdclass={
         "build_py": SmartSimBuild,
