2da5548fee0383f9098cbf5806285fc6fc6cbbba
Matt Drozt <drozt@hpe.com>
2024-07-23 12:15:10 -0500

    Headers/Marks/Boilerplate to test files for smartsim refactor

--- tests/temp_tests/test_core/test_commands/test_command.py
+++ tests/temp_tests/test_core/test_commands/test_command.py
@@ -24,8 +24,12 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import pytest
+
 from smartsim._core.commands.command import Command
 from smartsim.settings.launchCommand import LauncherType
+
+pytestmark = pytest.mark.group_a
 
 
 def test_command_init():
