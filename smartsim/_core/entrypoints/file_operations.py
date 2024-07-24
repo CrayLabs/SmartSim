92be5a35a5fea1788be770af6590ad6e381a387b
Julia Putko <putko@osprey.us.cray.com>
2024-07-24 12:58:01 -0500

    from __future__ import annotations to fix mypy error for python<3.10

--- smartsim/_core/entrypoints/file_operations.py
+++ smartsim/_core/entrypoints/file_operations.py
@@ -24,6 +24,8 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from __future__ import annotations
+
 import argparse
 import base64
 import functools
