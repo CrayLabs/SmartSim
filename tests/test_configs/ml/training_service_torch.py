87733d9e40b8987d93a573cb1fceed6c150d4f20
Al Rigazzi <al.rigazzi@hpe.com>
2025-06-09 19:31:37 +0200

    Make style

--- tests/test_configs/ml/training_service_torch.py
+++ tests/test_configs/ml/training_service_torch.py
@@ -26,6 +26,7 @@
 
 import os
 import platform
+
 import psutil
 import torch
 import torch.nn as nn
@@ -104,7 +105,7 @@ if __name__ == "__main__":
     print("Finished Training")
     del trainloader
 
-    if platform.machine() == 'arm64' and platform.system() == 'Darwin':
+    if platform.machine() == "arm64" and platform.system() == "Darwin":
         parent = psutil.Process(os.getpid())
         for child in parent.children(recursive=True):
             child.kill()
