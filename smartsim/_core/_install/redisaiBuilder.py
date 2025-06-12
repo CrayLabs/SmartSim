6945f92278142fe4b27c7188e80a45c2f1f1e3b0
Al Rigazzi <al.rigazzi@hpe.com>
2025-06-12 15:52:12 +0200

    Make style

--- smartsim/_core/_install/redisaiBuilder.py
+++ smartsim/_core/_install/redisaiBuilder.py
@@ -59,7 +59,9 @@ class RedisAIBuilder:
         build_env: BuildEnv,
         main_build_path: pathlib.Path,
         verbose: bool = False,
-        source: t.Union[str, pathlib.Path] = "https://github.com/RedisAI/redis-inference-optimization.git",
+        source: t.Union[
+            str, pathlib.Path
+        ] = "https://github.com/RedisAI/redis-inference-optimization.git",
         version: str = "v1.2.7",
     ) -> None:
 
