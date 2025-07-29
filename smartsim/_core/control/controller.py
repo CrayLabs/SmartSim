4329ab58e4209a7d4ed167077accd1f1b9469b7c
Al Rigazzi <al.rigazzi@hpe.com>
2025-07-29 14:51:57 +0200

    Remove obsolete lines

--- smartsim/_core/control/controller.py
+++ smartsim/_core/control/controller.py
@@ -373,10 +373,6 @@ class Controller:
         historical_err.touch()
         historical_out.touch()
 
-        # Ensure the entity directory exists for symlinks
-        # entity_out.parent.mkdir(parents=True, exist_ok=True)
-        # entity_err.parent.mkdir(parents=True, exist_ok=True)
-
         if historical_err.exists() and historical_out.exists():
             entity_out.symlink_to(historical_out)
             entity_err.symlink_to(historical_err)
