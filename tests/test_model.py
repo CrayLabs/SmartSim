b91cca2d98a26b674142e62b939d40659add3136
Al Rigazzi <al.rigazzi@hpe.com>
2025-08-15 10:04:27 +0200

    Remove comments

--- tests/test_model.py
+++ tests/test_model.py
@@ -31,7 +31,6 @@ import pytest
 
 from smartsim import Experiment
 
-# Removed LaunchedManifestBuilder import since it was deleted
 from smartsim._core.launcher.step import SbatchStep, SrunStep
 from smartsim.entity import Ensemble, Model
 from smartsim.entity.model import _parse_model_parameters
@@ -98,7 +97,6 @@ def monkeypatch_exp_controller(monkeypatch):
             self, exp_name, exp_path, manifest, block=True, kill_on_interrupt=True
         ):
             self._launch(exp_name, exp_path, manifest)
-            # Controller start method now returns None after LaunchedManifest removal
             return None
 
         def launch_step_nop(self, step, entity):
