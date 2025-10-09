9826d08d8c853f70050307751b898554f6b68869
balin <riccardo.balin@colorado.edu>
2025-10-09 18:11:22 +0000

    Add monitor argument to start_wo_job_manager()

--- tests/test_model.py
+++ tests/test_model.py
@@ -94,7 +94,13 @@ def monkeypatch_exp_controller(monkeypatch):
         entity_steps = []
 
         def start_wo_job_manager(
-            self, exp_name, exp_path, manifest, block=True, kill_on_interrupt=True
+            self,
+            exp_name,
+            exp_path,
+            manifest,
+            block=True,
+            kill_on_interrupt=True,
+            monitor=True,
         ):
             self._launch(exp_name, exp_path, manifest)
             return LaunchedManifestBuilder("name", "path", "launcher").finalize()
