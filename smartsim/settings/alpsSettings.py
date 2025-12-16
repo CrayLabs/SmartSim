0db712b7230a0b92572acb9ce7a0ca05aec4c4d9
Al Rigazzi <al.rigazzi@hpe.com>
2025-12-16 09:49:22 +0100

    Update smartsim/settings/alpsSettings.py
    Co-authored-by: Matt Drozt <matthew.drozt@gmail.com>

--- smartsim/settings/alpsSettings.py
+++ smartsim/settings/alpsSettings.py
@@ -36,7 +36,7 @@ class AprunSettings(RunSettings):
     def __init__(
         self,
         exe: str,
-        exe_args: t.Optional[str | list[str]] = None,
+        exe_args: str | list[str] | None = None,
         run_args: dict[str, int | str | float | None] | None = None,
         env_vars: dict[str, str | None] | None = None,
         **kwargs: t.Any,
