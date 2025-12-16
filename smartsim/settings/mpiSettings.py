62328f776a99c99025b21f33d741fdf8bf3741b4
Al Rigazzi <al.rigazzi@hpe.com>
2025-12-16 09:49:55 +0100

    Update smartsim/settings/mpiSettings.py
    Co-authored-by: Matt Drozt <matthew.drozt@gmail.com>

--- smartsim/settings/mpiSettings.py
+++ smartsim/settings/mpiSettings.py
@@ -327,7 +327,7 @@ class OrterunSettings(_BaseMPISettings):
     def __init__(
         self,
         exe: str,
-        exe_args: t.Optional[str | list[str]] = None,
+        exe_args: str | list[str] | None = None,
         run_args: dict[str, int | str | float | None] | None = None,
         env_vars: dict[str, str | None] | None = None,
         **kwargs: t.Any,
