74892ab97948e8ec46dba707e6658b240095c2a2
Al Rigazzi <al.rigazzi@hpe.com>
2025-12-16 09:50:20 +0100

    Update smartsim/settings/slurmSettings.py
    Co-authored-by: Matt Drozt <matthew.drozt@gmail.com>

--- smartsim/settings/slurmSettings.py
+++ smartsim/settings/slurmSettings.py
@@ -42,7 +42,7 @@ class SrunSettings(RunSettings):
     def __init__(
         self,
         exe: str,
-        exe_args: t.Optional[str | list[str]] = None,
+        exe_args: str | list[str] | None = None,
         run_args: dict[str, int | str | float | None] | None = None,
         env_vars: dict[str, str | None] | None = None,
         alloc: str | None = None,
